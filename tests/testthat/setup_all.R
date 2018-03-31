93a7231e92331ea49709a1c43f0b095a5b86b516
rhelmus <r.helmus@uva.nl>
2018-03-31 16:40:26 +0200

    optional enviPick tests

--- tests/testthat/setup_all.R
+++ tests/testthat/setup_all.R
@@ -1,14 +1,21 @@
 unlink(getWorkPath(), TRUE)
 dir.create(getWorkPath())
 
-exDataFiles <- list.files(patRoonData::exampleDataPath(), "\\.mzML$", full.names = TRUE)
-file.copy(exDataFiles, getWorkPath())
+runWithEnviPick <- FALSE
 
-testAnaInfo <- generateAnalysisInfo(getWorkPath(),
+testAnaInfo <- generateAnalysisInfo(patRoonData::exampleDataPath(),
                                     groups = c(rep("solvent", 3), rep("standard", 3)),
                                     refs = "solvent")
 
-convertMSFiles(testAnaInfo, "mzML", "mzXML")
+if (runWithEnviPick)
+{
+    exDataFiles <- list.files(patRoonData::exampleDataPath(), "\\.mzML$", full.names = TRUE)
+    convertMSFiles(testAnaInfo, "mzML", "mzXML", getWorkPath())
+    
+    testAnaInfoMzXML <- generateAnalysisInfo(getWorkPath(),
+                                             groups = c(rep("solvent", 3), rep("standard", 3)),
+                                             refs = "solvent")
+}
 
 if (!file.exists(getTestDataPath()))
     dir.create(getTestDataPath())
--- tests/testthat/test-features.R
+++ tests/testthat/test-features.R
@@ -2,16 +2,20 @@ context("features")
 
 ffOpenMS <- findFeatures(testAnaInfo, "openms", logPath = NULL)
 ffXCMS <- findFeatures(testAnaInfo, "xcms")
-ffEP <- findFeatures(testAnaInfo, "envipick")
+
+if (runWithEnviPick)
+    ffEP <- findFeatures(testAnaInfoMzXML, "envipick")
 
 test_that("verify feature finder output", {
     expect_known_value(featureTable(ffOpenMS), testFile("ff-openms"))
     expect_known_value(featureTable(ffXCMS), testFile("ff-xcms"))
+    skip_if_not(runWithEnviPick)
     expect_known_value(featureTable(ffEP), testFile("ff-envipick"))
 })
 
 test_that("verify show output", {
     expect_known_output(show(ffOpenMS), testFile("ff-show-openms", text = TRUE))
     expect_known_output(show(ffXCMS), testFile("ff-show-xcms", text = TRUE))
+    skip_if_not(runWithEnviPick)
     expect_known_output(show(ffEP), testFile("ff-show-envipick", text = TRUE))
 })

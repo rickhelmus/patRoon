d352f21502d2016641da347df8b2edd306328b12
rhelmus <r.helmus@uva.nl>
2018-04-04 22:12:04 +0200

    tests for screening

---
+++ tests/testthat/test-screening.R
@@ -0,0 +1,13 @@
+context("screening")
+
+fGroups <- getTestFGroups(getTestAnaInfo())
+scr <- screenTargets(fGroups, patRoonData::targets)
+scrF <- screenTargets(getFeatures(fGroups), patRoonData::targets)
+
+test_that("target screening is OK", {
+    expect_equal(nrow(scr), nrow(patRoonData::targets))
+    expect_length(unique(scrF$name), nrow(patRoonData::targets))
+    expect_known_value(scr, testFile("screening"))
+    expect_known_value(scrF, testFile("screening-feat"))
+    expect_length(groupFeaturesScreening(fGroups, scr), nrow(patRoonData::targets))
+})

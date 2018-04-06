2079d4d35e780337c169893b799e93ebfadf7d80
rhelmus <r.helmus@uva.nl>
2018-04-07 01:37:50 +0200

    ignore irreproducible intensities

--- tests/testthat/test-screening.R
+++ tests/testthat/test-screening.R
@@ -8,6 +8,6 @@ test_that("target screening is OK", {
     expect_equal(nrow(scr), nrow(patRoonData::targets))
     expect_length(unique(scrF$name), nrow(patRoonData::targets))
     expect_known_value(scr, testFile("screening"))
-    expect_known_value(scrF, testFile("screening-feat"))
+    expect_known_value(scrF[, -"feature"], testFile("screening-feat"))
     expect_length(groupFeaturesScreening(fGroups, scr), nrow(patRoonData::targets))
 })

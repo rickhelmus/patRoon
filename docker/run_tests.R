5c1c6d713aa4cb3e347230a168d75324f69887bb
rhelmus <r.helmus@uva.nl>
2018-04-22 12:13:42 +0200

    junit output

--- docker/run_tests.R
+++ docker/run_tests.R
@@ -8,7 +8,7 @@ devtools::install(upgrade_dependencies = FALSE)
 file.rename("tests/figs/deps-docker.txt", "tests/figs/deps.txt")
 
 # return failure exit code when tests fail: https://github.com/r-lib/testthat/issues/515
-tret <- as.data.frame(devtools::test())
+tret <- as.data.frame(devtools::test(reporter = JunitReporter$new(file = "junit.xml")))
 print(tret)
 if (sum(tret$failed) > 0 || any(tret$error))
     q(status = 1)

1ed2cd0473c972dd78cee9e8193de7646e098fa1
Rick Helmus <r.helmus@uva.nl>
2018-08-26 20:44:46 +0200

    show EICs, hide/show compound table, various tweaks

--- inst/js/utils-report.js
+++ inst/js/utils-report.js
@@ -5,18 +5,19 @@ function getAnnotationPageElement()
 
 function showAnnotation(group, type)
 {
-    $('#compoundsTable .dataTable').DataTable().column(0).search(group).draw();
-    return;
-    
     clearComponentSpecs();
     
-    var comps = document.getElementsByClassName('annotationClass');
-    var annotationId = type + "_" + "ann-" + group;
-    var EICId = "EIC-" + group;
-    for (var i=0; i<comps.length; i++)
-        comps[i].style.display = (comps[i].classList.contains(annotationId) || comps[i].classList.contains(EICId)) ? 'flex' : 'none';
-
-    document.getElementById("noAnnotationSelected").style.display = 'none';
+    const annElements = document.getElementsByClassName('annotationClass');
+    const EICId = "EIC-" + group;
+    for (var i=0; i<annElements.length; i++)
+    {
+        annElements[i].style.display = (annElements[i].classList.contains(type) ||
+                                        annElements[i].classList.contains(EICId)) ? 'flex' : 'none';
+    }
+    
+    document.getElementsByClassName("noAnnotationSelected")[0].style.display = 'none';
+    $('#compoundsTable .dataTable').DataTable().column(0).search(group).draw();
+    //$('.compounds').show();
 }
 
 function showComponentSpec(component, parentID)
@@ -47,5 +48,6 @@ function disableAllAnnotations()
 
 $(document).ready(function()
 {
-    //     setTimeout(disableAllSpecs, 500); // HACK: wait a bit so that HTML instances are available
+    //setTimeout(function() { $('.annotationClass').hide(); }, 1500);
+    setTimeout(disableAllAnnotations, 5); // HACK: wait a bit so that HTML instances are available
 });

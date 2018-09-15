f61c61d6cca1e5b141ab26add7720e83dba41ee5
rhelmus <r.helmus@uva.nl>
2018-09-15 12:18:51 +0200

    handle methods with multiple classes in their signature

--- man-roxygen/generics.R
+++ man-roxygen/generics.R
@@ -1,8 +1,12 @@
+# automatically retrieve defined methods for a generic and create document
+# links. This only works if the arguments of the method are named obj or objX.
+
 #' @details \code{<%=func%>} <%=desc%>
 #'
 #' <% cl <- showMethods(func, where = "package:patRoon", printTo = FALSE) %>
-#' <% cl <- cl[grepl("obj=", cl)] %>
-#' <% cl <- sub("obj=\"([[:alpha:]]+)\"", "\\1", cl) %>
+#' <% cl <- cl[grepl("obj.?=", cl)] %>
+#' <% cl <- gsub("obj.?=\"([[:alpha:]]+)\"", "\\1", cl) %>
+#' <% cl <- gsub(" ", "", cl) %>
 #'
 #' \itemize{
 #'     \item Methods are defined for: <%= { paste0(sprintf("\\code{\\link[=%s,%s-method]{%s}}", func, cl, cl), collapse = ", ") } %>.

4d23b76c8d3632e9c6a1893389d6501d762c9766
rhelmus <r.helmus@uva.nl>
2018-10-22 22:37:48 +0200

    enviPick optimizer class

--- R/features-optimize-envipick.R
+++ R/features-optimize-envipick.R
@@ -1,5 +1,31 @@
 #' @include main.R
+#' @include features-optimize.R
 NULL
+
+featuresOptimizerEnviPick <- setRefClass("featuresOptimizerEnviPick", contains = "featuresOptimizer")
+
+featuresOptimizerEnviPick$methods(
+    
+    fixOptParamBounds = function(param, bounds)
+    {
+        if (param %in% c("minpeak", "ended", "recurs"))
+            return(round(bounds, 0))
+        
+        return(bounds)
+    },
+    
+    fixOptParams = function(params)
+    {
+        return(fixOptParamRange(params, list(c("minint", "maxint"))))
+    },
+    
+    getMinOptSetting = function(settingName, params)
+    {
+        return(1)
+    }
+)
+
+
 
 convertOptToCallParamsEnviPick <- function(params)
 {

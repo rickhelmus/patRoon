83e92279f7bfca24e01208cd3116ca01cde77215
rhelmus <r.helmus@uva.nl>
2018-11-22 21:00:52 +0100

    update vdiffr refs and don't use separate set anymore for Docker

--- docker/run_tests.R
+++ docker/run_tests.R
@@ -1,5 +1,5 @@
 # unfortunately vdiffr doesn't allow to specify the deps file name.
-file.rename("tests/figs/deps-docker.txt", "tests/figs/deps.txt")
+# file.rename("tests/figs/deps-docker.txt", "tests/figs/deps.txt")
 
 # return failure exit code when tests fail: https://github.com/r-lib/testthat/issues/515
 tret <- as.data.frame(devtools::test(reporter = MultiReporter$new(list(SummaryReporter$new(),

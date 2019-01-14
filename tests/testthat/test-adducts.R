30c4300c010aba567a7133ed5053e87a7401c73e
rickhelmus <r.helmus@uva.nl>
2019-01-14 21:57:29 +0100

    fixes adduct tests

--- tests/testthat/test-adducts.R
+++ tests/testthat/test-adducts.R
@@ -4,9 +4,10 @@ test_that("adduct class and utilities", {
     expect_equal(adduct("H"), as.adduct("[M+H]+"))
     expect_equal(adduct("H"), as.adduct("[M+H]+"))
     expect_equal(adduct(sub = "H", charge = -1), as.adduct("[M-H]-"))
-    expect_equal(adduct(add = "Na", sub = "2H", charge = -1), as.adduct("[M-2H+Na]-"))
-    expect_equal(adduct(add = "2H", charge = 2), as.adduct("[M+2H]2+"))
+    expect_equal(adduct(add = "Na", sub = "H2", charge = -1), as.adduct("[M-H2+Na]-"))
+    expect_equal(adduct(add = "H2", charge = 2), as.adduct("[M+H2]2+"))
     expect_equal(adduct(add = "H", molMult = 3), as.adduct("[3M+H]+"))
+    expect_equal(adduct(add = "COOH"), as.adduct("[M+CHO2]+"))
 
     expect_equal(adduct(add = "H"), as.adduct("[M+H]+", format = "metfrag"))
     expect_equal(adduct(add = "H"), as.adduct(1, format = "metfrag", isPositive = TRUE))
@@ -18,8 +19,8 @@ test_that("adduct class and utilities", {
                            FUN = function(a, ch) is(as.adduct(a, format = "metfrag", isPositive = ch>0), "adduct"))))
 
     expect_equal(as.character(adduct("H")), "[M+H]+")
-    expect_equal(as.character(adduct(add = "Na", sub = "2H", charge = -1)), "[M+Na-2H]-")
-    expect_equal(as.character(adduct(add = "2Na", charge = 2)), "[M+2Na]2+")
+    expect_equal(as.character(adduct(add = "Na", sub = "H2", charge = -1)), "[M+Na-H2]-")
+    expect_equal(as.character(adduct(add = "Na2", charge = 2)), "[M+Na2]2+")
 
     expect_equal(calculateNeutralFormula(calculateIonFormula("C2H4O", "[M+H]+"), "[M+H]+"), "C2H4O")
 })

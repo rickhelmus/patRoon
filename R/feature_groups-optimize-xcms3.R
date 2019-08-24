9f4706170d87e4e70865946f5691c6a71c075b77
Rick Helmus <r.helmus@uva.nl>
2019-08-24 21:54:25 +0200

    updates on XCM3 grouping optim

--- R/feature_groups-optimize-xcms3.R
+++ R/feature_groups-optimize-xcms3.R
@@ -1,191 +1,170 @@
 #' @include main.R
-#' @include feature_groups-optimize-xcms.R
+#' @include feature_groups-optimize.R
 NULL
 
-# UNDONE: do the subset/subsetAdjust/minFraction parameters currently work for ret correction?
-
-convertXCMS3GroupListToParam <- function(args, sGroups)
+convertXCMS3GroupListToParam <- function(args, method, sGroups)
 {
-    method <- args$method
-    args <- args[names(args) != "method"]
     args$sampleGroups <- sGroups
     
     if (method == "density")
-    {
-        # update to newstyle naming
-        args <- setListNamesIfPresent(args,
-                                      c("minfrac", "minsamp", "mzwid", "max"),
-                                      c("minFraction", "minSamples", "binSize", "maxFeatures"))
         return(do.call(xcms::PeakDensityParam, args))
-    }
-    
     if (method == "nearest")
-    {
-        args <- setListNamesIfPresent(args,
-                                      c("mzCheck", "rtCheck"),
-                                      c("absMz", "absRt"))
         return(do.call(xcms::NearestPeaksParam, args))
-    }
-    
-    if (method == "mzClust") # UNDONE: not really supported in XCMS base class
-    {
-        args <- setListNamesIfPresent(args,
-                                      c("mzppm", "mzabs", "minsamp", "minfrac"),
-                                      c("ppm", "absMz", "minSamples", "minFraction"))
+    if (method == "mzClust") # UNDONE: support this?
         return(do.call(xcms::MzClustParam, args))
-    }
+    
+    stop(paste("Unknown/Unsupported grouping method:", method))
 }
 
-convertXCMS3RetAlignListToParam <- function(args)
+convertXCMS3RetAlignListToParam <- function(args, method)
 {
-    method <- args$method
-    args <- args[names(args) != "method"]
-    
     if (method == "obiwarp")
-    {
-        args <- setListNamesIfPresent(args,
-                                      c("profStep", "center", "distFunc"),
-                                      c("binSize", "centerSample", "distFun"))
         return(do.call(xcms::ObiwarpParam, args))
-    }
-    
     if (method == "peakgroups")
-    {
-        # UNDONE: old style 'missing' param doesn't translate
-        args <- setListNamesIfPresent(args,
-                                      c("extra"),
-                                      c("extraPeaks"))
         return(do.call(xcms::PeakGroupsParam, args))
-    }
-}
-
-convertXCMS3GroupParamToList <- function(param)
-{
-    if (isXCMSClass(param, "PeakDensityParam"))
-        return(list(minfrac = xcms::minFraction(param),
-                    minsamp = xcms::minSamples(param),
-                    bw = xcms::bw(param),
-                    mzwid = xcms::binSize(param),
-                    max = xcms::maxFeatures(param)))
     
-    if (isXCMSClass(param, "NearestPeaksParam"))
-        return(list(mzVsRTbalance = xcms::mzVsRtBalance(param),
-                    mzCheck = xcms::absMz(param),
-                    rtCheck = xcms::absRt(param),
-                    kNN = xcms::kNN(param)))
-    
-    if (isXCMSClass(param, "MzClustParam"))
-        return(list(mzppm = xcms::ppm(param),
-                    mzabs = xcms::absMz(param),
-                    minsamp = xcms::minSamples(param),
-                    minfrac = xcms::minFraction(param)))
+    stop(paste("Unknown/Unsupported retention alignment method:", method))
 }
 
-convertXCMS3RetAlignParamToList <- function(param)
-{
-    if (isXCMSClass(param, "ObiwarpParam"))
-        return(list(profStep = xcms::binSize(param),
-                    center = xcms::centerSample(param),
-                    response = xcms::response(param),
-                    distFunc = xcms::distFun(param),
-                    gapInit = xcms::gapInit(param),
-                    gapExtend = xcms::gapExtend(param),
-                    factorDiag = xcms::factorDiag(param),
-                    factorGap = xcms::factorGap(param),
-                    localAlignment = xcms::localAlignment(param),
-                    initPenalty = xcms::initPenalty(param)))
-    
-    if (isXCMSClass(param, "peakgroups"))
-        return(list(extra = xcms::extraPeaks(param),
-                    smooth = xcms::smooth(param),
-                    span = xcms::span(param),
-                    family = xcms::family(param)))
-}
-
-# Split the ranges for each entry over two lists, so we can convert it to
-# lower/upper param objects.
-splitXCMSOptRangeArgs <- function(args)
-{
-    rangeArgs <- lengths(args) == 2
-    lower <- lapply(args[rangeArgs], "[[", 1)
-    upper <- lapply(args[rangeArgs], "[[", 2)
-    other <- args[!rangeArgs]
-    return(list(c(other, lower), c(other, upper)))
-}
-
-featureGroupsOptimizerXCMS3 <- setRefClass("featureGroupsOptimizerXCMS3", contains = "featureGroupsOptimizerXCMS")
+featureGroupsOptimizerXCMS3 <- setRefClass("featureGroupsOptimizerXCMS3", contains = "featureGroupsOptimizer",
+                                           fields = list(groupParamNames = "character", retAlignParamNames = "character"))
 
 featureGroupsOptimizerXCMS3$methods(
-    
-    convertOptToCallParams = function(params)
+
+    flattenParams = function(params, setNames)
     {
-        params <- callSuper(params)
-        ret <- list()
+        # combine groupParams and retAlignParams in one list for easier optimization
         
-        if (!is.null(params$groupArgs[["method"]]))
-            ret$groupParam <- convertXCMS3GroupListToParam(params$groupArgs,
-                                                           analysisInfo(features)$group)
+        # filter out invalid params (e.g. when user forgets to use
+        # groupParams/retAlignParams for specifying params to optimize)
+        params <- params[names(params) %in% c("rtalign", "groupMethod", "groupParams", "retAlignMethod", "retAlignParams")]
 
-        if (!is.null(params$retcorArgs[["method"]]))
-            ret$retAlignParam <- convertXCMS3RetAlignListToParam(params$retcorArgs)
-
-        return(ret)
+        if (is.null(params[["groupParams"]]))
+            params$groupParams <- list()
+        if (is.null(params[["retAlignParams"]]))
+            params$retAlignParams <- list()
+        
+        if (setNames)
+        {
+            # save which parameters belong to where so we can re-construct the param objects later
+            groupParamNames <<- if (length(params$groupParams) > 0) names(params$groupParams) else character()
+            retAlignParamNames <<- if (length(params$retAlignParams) > 0) names(params$retAlignParams) else character()
+        }
+        
+        # flatten
+        params <- c(params, params[["groupParams"]], params[["retAlignParams"]])
+        params[c("groupParams", "retAlignParams")] <- NULL # and remove original sublists
+        
+        return(params)
     },
     
     checkInitialParams = function(params)
     {
-        # params should be a list containing:
-        # - groupParam: a *list* with two xcms group param objects, specifying lower/upper bounds for grouping
-        # - retAlignParam: as above, but for alignment instead of grouping
-        #
-        # These objects are converted to a list format the optimizer can use (or
-        # actually the old-style XCMS optimizer can use). This is done by
-        # merging the lower/upper values after conversion to a list object.
-
-        mergeArgList <- function(lower, upper)
+        if (is.null(params[["groupMethod"]]))
+            params$groupMethod <- "density"
+        if (is.null(params[["retAlignMethod"]]))
+            params$retAlignMethod <- "obiwarp"
+        return(flattenParams(params, TRUE))
+    },
+    
+    getOptSettingRange = function(settingName, params, paramRanges) callSuper(settingName, params, flattenParams(paramRanges, FALSE)),
+    
+    fixOptParamBounds = function(param, bounds)
+    {
+        if (param == "extraPeaks")
+            return(round(bounds, 0))
+        if (param %in% c("binSize", "minFraction"))
         {
-            # combine lists first: this ensures unique values in either are kept.
-            ret <- modifyList(lower, upper)
-            
-            inBoth <- intersect(names(lower), names(upper))
-            ret[inBoth] <- mapply(lower[inBoth], upper[inBoth], SIMPLIFY = FALSE, FUN = c)
-
-            return(ret)
+            if (bounds[2] > 1)
+            {
+                # 1 is max value
+                bounds <- round(c(1-(diff(bounds)*0.8), 1), 2)
+                printf("binSize or minFraction greater 1, decreasing to %s\n", bounds)
+            }
         }
-                
-        ret <- list(groupArgs = mergeArgList(convertXCMS3GroupParamToList(params$groupParam[[1]]),
-                                             convertXCMS3GroupParamToList(params$groupParam[[2]])),
-                    retcorArgs = mergeArgList(convertXCMS3RetAlignParamToList(params$retAlignParam[[1]]),
-                                              convertXCMS3RetAlignParamToList(params$retAlignParam[[2]])))
         
-        return(callSuper(ret))
+        return(bounds)
+    },
+    
+    convertOptToCallParams = function(params)
+    {
+        ret <- list()
+        
+        if (!is.null(params[["rtalign"]]))
+            ret$rtalign <- params$rtalign
+
+        # convert flattened group/ret align lists back to XCMS usable param objects
+        ret$groupParam <- convertXCMS3GroupListToParam(params[groupParamNames], params$groupMethod,
+                                                       analysisInfo(features)$group)
+        ret$retAlignParam <- convertXCMS3RetAlignListToParam(params[retAlignParamNames],
+                                                             params$retAlignMethod)
+        
+        return(ret)
     }
 )
 
-generateFGroupsOptPSetXCMS3 <- function(..., features)
+generateFGroupsOptPSetXCMS3 <- function(...)
 {
     givenArgs <- list(...)
     
-    if (!is.null(givenArgs[["groupParam"]]) || !is.null(givenArgs[["retAlignParam"]]))
-        stop(paste0("Overriding default settings for grouping/retention alignment currently not supported!",
-                    "Please modify these values after the parameter set has been created."))
+    groupMethod <- givenArgs[["groupMethod"]]
+    if (is.null(groupMethod))
+        groupMethod <- "density"
     
-    # the defaults are based on the old style XCMS base class
-    args <- generateFGroupsOptPSetXCMS(...)
+    retAlignMethod <- givenArgs[["retAlignMethod"]]
+    if (is.null(retAlignMethod))
+        retAlignMethod <- "obiwarp"
     
-    return(list(groupParam = lapply(splitXCMSOptRangeArgs(args$groupArgs), convertXCMS3GroupListToParam,
-                                    sGroups = analysisInfo(features)$group),
-                retAlignParam = lapply(splitXCMSOptRangeArgs(args$retcorArgs), convertXCMS3RetAlignListToParam)))
+    groupParams <- retAlignParams <- list()
+    
+    if (groupMethod == "density")
+    {
+        groupParams$bw <- c(22, 28)
+        groupParams$binSize <- c(0.015, 0.035)
+    }
+    else if (groupMethod == "nearest")
+    {
+        # UNDONE: check if this makes sense (does this grouping method?)
+        groupParams$absMz <- c(0.015, 0.035)
+        groupParams$absRt <- c(5, 15)
+    }
+    else
+        stop("Only density and nearest group methods supported.")
+    
+    if (is.null(givenArgs[["rtalign"]]) || givenArgs[["rtalign"]])
+    {
+        if (retAlignMethod == "obiwarp")
+        {
+            distFun <- givenArgs[["retAlignParams"]][["distFun"]]
+            if (is.null(distFun))
+            {
+                distFun <- "cor_opt"
+                # only set when not specified, otherwise it will be doubly set in generateFGroupsOptPSet()
+                retAlignParams$distFun <- distFun
+            }
+            
+            retAlignParams[c("gapInit", "gapExtend")] <- switch(distFun,
+                                                                cor = list(c(0, 0.4), c(2.1, 2.7)),
+                                                                cor_opt = list(c(0, 0.4), c(2.1, 2.7)),
+                                                                cov = list(c(0, 0.4), c(11.4, 12)),
+                                                                prd = list(c(0, 0.4), c(7.5, 8.1)),
+                                                                euc = list(c(0.7, 1.1), c(1.5, 2.1)))
+            retAlignParams$binSize <- c(0.7, 1)
+        }
+        else if (retAlignMethod == "peakgroups")
+        {
+            retAlignParams$extraPeaks <- c(1, 3)
+            retAlignParams$span <- c(0.1, 0.3)
+        }
+        else
+            stop("Only obiwarp and peakgroups alignment methods are supported.")
+    }
+    
+    return(list(groupParams = groupParams, retAlignParams = retAlignParams))
 }
 
-getDefFGroupsOptParamRangesXCMS3 <- function(features)
+getDefFGroupsOptParamRangesXCMS3 <- function()
 {
-    args <- getDefFGroupsOptParamRangesXCMS()
-    
-    # necessary for conversion to param objects
-    args$groupArgs$method <- "density"; args$retcorArgs$method <- "obiwarp"
-    
-    return(list(groupParam = lapply(splitXCMSOptRangeArgs(args$groupArgs), convertXCMS3GroupListToParam,
-                                    sGroups = analysisInfo(features)$group),
-                retAlignParam = lapply(splitXCMSOptRangeArgs(args$retcorArgs), convertXCMS3RetAlignListToParam)))
+    return(list(groupParams = list(binSize = c(0.0001, Inf), bw = c(0.25, Inf)),
+                retAlignParams = list(binSize = c(0.3, Inf), span = c(0.001, Inf))))
 }

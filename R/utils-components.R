41eed3c3f01360a39f5142ff108b986c556c9a91
Rick Helmus <r.helmus@uva.nl>
2019-10-22 15:59:36 +0200

    plotGraph for componentsTPs

--- R/utils-components.R
+++ R/utils-components.R
@@ -50,3 +50,49 @@ calculateComponentIntensities <- function(comps, fGroups)
         return(cmp[])
     }))
 }
+
+makeGraph <- function(components, onlyLinked, titles)
+{
+    cInfo <- copy(components@componentInfo)
+    
+    # convert link IDs to numeric indices
+    cInfo[, id := .I]
+    cInfo[, linksIDs := lapply(links, match, table = names(components))]
+    allLinks <- unique(unlist(cInfo$linksIDs))
+    cInfo <- cInfo[lengths(links) > 0 | id %in% allLinks]
+    
+    if (nrow(cInfo) == 0)
+    {
+        if (onlyLinked)
+            stop("No component links in your data and onlyLinked = TRUE, so nothing to show.")
+        nodes <- data.table(id = character(), label = character(), group = numeric())
+        edges <- data.table(from = character(), to = character())
+    }
+    else
+    {
+        edges <- rbindlist(mapply(cInfo$name, cInfo$linksIDs, FUN = function(n, l)
+        {
+            data.table::data.table(from = n, to = cInfo$name[match(unlist(l), cInfo$id)])
+        }, SIMPLIFY = FALSE))
+        
+        graph <- igraph::simplify(igraph::graph_from_data_frame(edges, directed = FALSE))
+        fc <- igraph::fastgreedy.community(graph)
+        
+        data <- visNetwork::toVisNetworkData(graph)
+        nodes <- as.data.table(data$nodes)
+        nodes[, group := fc$membership]
+        edges <- data$edges
+    }
+
+    if (!onlyLinked)
+    {
+        unNodes <- data.table(id = setdiff(names(components), cInfo$name), group = 0)
+        unNodes[, label := id]
+        nodes <- rbind(nodes, unNodes)
+    }
+    
+    nodes[, shape := "circle"]
+    nodes[, title := titles[match(id, names(components))]]
+    
+    visNetwork::visNetwork(nodes = nodes, edges = data$edges)
+}
---
+++ man-roxygen/plotGraph.R
@@ -0,0 +1,5 @@
+#' @param onlyLinked If \code{TRUE} then only components with links are plotted.
+#'
+#' @return \code{plotGraph} returns the result of \code{\link{visNetwork}}.
+#'
+#' @references \addCitations{igraph}{1} \cr \cr \addCitations{visNetwork}{1}

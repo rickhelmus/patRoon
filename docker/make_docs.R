3df4fe2330237a21eee981aea8862e36c4edc8bc
Rick Helmus <r.helmus@uva.nl>
2020-03-31 13:28:13 +0200

    autodoc trial 7 [skip appveyor]

--- docker/make_docs.R
+++ docker/make_docs.R
@@ -5,7 +5,7 @@ pkgdown::build_site(examples = FALSE)
 
 # make book after pkgdown, otherwise it complains
 # NOTE: set clean_envir to FALSE so that 'out' variable below is recognized
-out <- normalizePath("docs", "handbook_bd", mustWork = FALSE)
+out <- file.path(normalizePath("docs", mustWork = FALSE), "handbook_bd")
 withr::with_dir("vignettes/handbook/", bookdown::render_book("_index.Rmd",
                                                              output_dir = out,
                                                              clean_envir = FALSE))

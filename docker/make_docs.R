8037a148d77f9c949eb3d0ce442fe8a6611d64fd
Rick Helmus <r.helmus@uva.nl>
2020-05-28 17:40:43 +0200

    updates for new bookdown/pkgdown adventures

--- docker/make_docs.R
+++ docker/make_docs.R
@@ -1,3 +1,15 @@
+# HACK HACK HACK: recently bookdown started to ignore files starting with
+# underscores. This mechanism is also used for pkgdown to ignore files, which we
+# need as it shouldn't include the bookdown sub-Rmd files in the website. For
+# now we temporarily re-name the files to start with underscores and rename back
+# once finished building the website...
+handbookSubRmdPath <- file.path("vignettes", "handbook")
+origRmds <- list.files(handbookSubRmdPath, pattern = "\\.Rmd")
+disabledRmds <- paste0("_", origRmds)
+origRmds <- file.path(handbookSubRmdPath, origRmds)
+disabledRmds <- file.path(handbookSubRmdPath, disabledRmds)
+file.rename(origRmds, disabledRmds)
+
 install.packages(c("pkgdown", "bookdown", "DiagrammeR", "rsvg", "tinytex", "webshot"))
 remotes::install_github("rich-iannone/DiagrammeRsvg")
 
@@ -7,6 +19,9 @@ Sys.setenv(PATH = paste0(Sys.getenv("PATH"), ":", "/home/patRoon/bin"))
 
 pkgdown::clean_site()
 pkgdown::build_site(examples = FALSE)
+
+# HACK HACK HACK: re-enable bookdown files again...
+file.rename(disabledRmds, origRmds)
 
 # make book after pkgdown, otherwise it complains
 # NOTE: set clean_envir to FALSE so that 'out' variable below is recognized

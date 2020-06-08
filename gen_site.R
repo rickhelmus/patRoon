704b2921005b473d4177e9d6f2f99845040d9e23
Rick Helmus <r.helmus@uva.nl>
2020-06-08 17:22:09 +0200

    cleanup [skip ci]

--- gen_site.R
+++
@@ -1,7 +0,0 @@
-docs <- normalizePath("docs")
-pkgdown::clean_site()
-pkgdown::build_site(examples = FALSE)
-
-# make book after pkgdown, otherwise it complains
-withr::with_dir("vignettes/handbook/", bookdown::render_book("_index.Rmd", output_dir = file.path(docs, "handbook_bd")))
-withr::with_dir("vignettes/handbook/", bookdown::render_book("_index.Rmd", "bookdown::pdf_book"))

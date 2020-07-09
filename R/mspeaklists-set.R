e5ad4bb7b984482d509104e496527b30ad373efa
Rick Helmus <r.helmus@uva.nl>
2020-07-09 13:01:58 +0200

    subsetting MSPeakListsSet

--- R/mspeaklists-set.R
+++ R/mspeaklists-set.R
@@ -29,11 +29,60 @@ neutralizeMSPeakLists <- function(MSPeakLists, adduct)
 
 #' @export
 MSPeakListsSet <- setClass("MSPeakListsSet",
-                           slots = c(setObjects = "list"),
+                           slots = c(setObjects = "list", ionizedPeakLists = "list",
+                                     analysisInfo = "data.frame"),
                            contains = "MSPeakLists")
 
 setMethod("initialize", "MSPeakListsSet",
           function(.Object, ...) callNextMethod(.Object, algorithm = "set", ...))
+
+#' @describeIn features Get analysis information
+#' @return \code{analysisInfo}: A \code{data.frame} containing a column with
+#'   analysis name (\code{analysis}), its path (\code{path}), and other columns
+#'   such as replicate group name (\code{group}) and blank reference
+#'   (\code{blank}).
+#' @export
+setMethod("analysisInfo", "MSPeakListsSet", function(obj) obj@analysisInfo)
+
+#' @describeIn MSPeakListsSet Subset on analyses/feature groups.
+#' @param reAverage Set to \code{TRUE} to regenerate averaged MS peak lists
+#'   after subsetting analyses.
+#' @export
+setMethod("[", c("MSPeakListsSet", "ANY", "ANY", "missing"), function(x, i, j, ..., reAverage = TRUE,
+                                                                      sets = NULL, drop = TRUE)
+{
+    ac <- checkmate::makeAssertCollection()
+    assertSets(x, sets, add = ac)
+    checkmate::assertFlag(reAverage, add = ac)
+    checkmate::reportAssertions(ac)
+    
+    if (!is.null(sets) && length(sets) > 0)
+        i <- mergeAnaSubsetArgWithSets(i, sets, analysisInfo(x))
+    
+    x <- callNextMethod(x, i, j, ..., reAverage = TRUE)
+
+    if (!missing(i))
+        x@analysisInfo <- x@analysisInfo[x@analysisInfo$analysis %in% names(peakLists(x)), ]
+    
+    if (!missing(i) || !missing(j))
+    {
+        args <- list(reAverage = reAverage)
+        if (!missing(i))
+            args <- c(args, list(i = analyses(x)))
+        if (!missing(j))
+            args <- c(args, list(j = groupNames(x)))
+        
+        # NOTE: assume that subsetting with non-existing i/j will not result in errors
+        x@setObjects <- lapply(x@setObjects, function(o) do.call("[", args = c(list(x = o), args)))
+        x@setObjects <- pruneList(x@setObjects, checkEmptyElements = TRUE)
+        
+        # re-generate
+        x@ionizedPeakLists <- Reduce(modifyList, lapply(x@setObjects, peakLists))
+    }
+    
+    return(x)
+})
+
 
 generateMSPeakListsSet <- function(fGroupsSet, generator, ..., avgSetParams)
 {
@@ -56,6 +105,8 @@ generateMSPeakListsSet <- function(fGroupsSet, generator, ..., avgSetParams)
     combPLIon <- Reduce(modifyList, lapply(ionizedMSPeakLists, peakLists))
     
     # UNDONE: set metadata?
-    return(MSPeakListsSet(setObjects = ionizedMSPeakLists, peakLists = combPL, metadata = list(),
+    return(MSPeakListsSet(setObjects = ionizedMSPeakLists, ionizedPeakLists = combPLIon,
+                          analysisInfo = analysisInfo(fGroupsSet),
+                          peakLists = combPL, metadata = list(),
                           avgPeakListArgs = avgSetParams, origFGNames = names(fGroupsSet)))
 }

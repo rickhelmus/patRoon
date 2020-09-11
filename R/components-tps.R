0200ad4b53e7d0293817fb283ef7bf3a4eec9129
Rick Helmus <r.helmus@uva.nl>
2020-09-11 11:12:42 +0200

    update generateComponentsTPs for new suspect screening interface and decoupled suspect screening

--- R/components-tps.R
+++ R/components-tps.R
@@ -49,35 +49,34 @@ setMethod("plotGraph", "componentsTPs", function(obj, onlyLinked)
 })
 
 #' @export
-generateComponentsTPs <- function(fGroups, pred, MSPeakLists, adduct, mzWindow = 0.005,
-                                  fGroupsPrec = fGroups, fGroupsTPs = fGroups, minRTDiff = 20)
+generateComponentsTPs <- function(fGroups, fGroupsTPs = NULL, pred, MSPeakLists, mzWindow = 0.005, minRTDiff = 20)
 {
+    # UNDONE: make method
     # UNDONE: optionally remove TPs with equal formula as parent (how likely are these?)
+    # UNDONE: optional MSPeakLists? and MSPeakListsTPs?
     
-    aapply(checkmate::assertClass, . ~ fGroups + fGroupsPrec + fGroupsTPs, "featureGroups")
-
-    rGroups <- replicateGroups(fGroups)
+    
     
     ac <- checkmate::makeAssertCollection()
-    checkmate::assertClass(pred, "TPPredictions")
-    checkmate::assertClass(MSPeakLists, "MSPeakLists")
-    
+    aapply(checkmate::assertClass, . ~ fGroups + fGroupsTPs + pred + MSPeakLists,
+           c("featureGroupsScreening", "featureGroupsScreening", "TPPredictions", "MSPeakLists"),
+           null.ok = c(FALSE, TRUE, FALSE, FALSE), fixed = list(add = ac))
     aapply(checkmate::assertNumber, . ~ mzWindow + minRTDiff,
            lower = 0, finite = TRUE, fixed = list(add = ac))
-    
     checkmate::reportAssertions(ac)
-    
-    adduct <- checkAndToAdduct(adduct)
-    
+
     if (length(fGroups) == 0)
         return(componentsTPs(componentInfo = data.table(), components = list()))
+
+    if (is.null(fGroupsTPs))
+        fGroupsTPs <- fGroups
     
-    hash <- makeHash(fGroups, pred, adduct, mzWindow, fGroupsPrec, fGroupsTPs, minRTDiff)
+    hash <- makeHash(fGroups, fGroupsTPs, pred, MSPeakLists, mzWindow, minRTDiff)
     cd <- loadCacheData("componentsTPs", hash)
     if (!is.null(cd))
         return(cd)
     
-
+    
     # for every precursor:
     #   - check for any matching fGroups (based on mass)
     #   - if none, skip
@@ -86,35 +85,28 @@ generateComponentsTPs <- function(fGroups, pred, MSPeakLists, adduct, mzWindow =
     #       - filter TPs (retention, intensity, ...)
     
     
-    suspList <- convertToSuspects(pred, includePrec = FALSE)
-
-    printf("Screening TPs...\n")
+    precFGMapping <- linkPrecursorsToFGroups(pred, fGroups)
+    gInfoPrec <- groupInfo(fGroups); gInfoTPs <- groupInfo(fGroupsTPs)
+    screeningTPs <- screenInfo(fGroupsTPs)
     
-    screeningTPs <- screenSuspects(fGroupsTPs, suspList, mzWindow = mzWindow, adduct = adduct)
-    screeningTPs <- screeningTPs[!is.na(group)] # remove non-hits
-    susps <- suspects(pred)
-    precGroups <- linkPrecursorsToFGroups(pred, fGroupsPrec, adduct, mzWindow)
-    
-    gInfo <- groupInfo(fGroups)
-    
-    compTab <- rbindlist(mapply(susps$name, predictions(pred), SIMPLIFY = FALSE, FUN = function(pname, preds)
+    compTab <- rbindlist(mapply(names(pred), predictions(pred), SIMPLIFY = FALSE, FUN = function(pname, preds)
     {
-        scrP <- precGroups[precGroups$name == pname]
+        precFGs <- precFGMapping[name == pname][["group"]]
         scrTP <- screeningTPs[name %in% preds$name]
         
-        if (nrow(scrP) == 0 || nrow(scrTP) == 0)
+        if (length(precFGs) == 0 || nrow(scrTP) == 0)
             return(NULL)
-
-        rnCols <- c("name", "mz", "exp_rt", "exp_mz")
+        
+        rnCols <- c("name", "mz")
         scrTP <- scrTP[, c("group", rnCols), with = FALSE]
-        setnames(scrTP, rnCols, c("TP_name", "TP_mz", "rt", "mz"))
+        setnames(scrTP, rnCols, c("TP_name", "TP_mz"))
         
         # limit columns a bit to not bloat components too much
         # UNDONE: column selection OK?
-        prCols <- c("name", "InChIKey", "formula", "mass", "RTDir")
-        preds <- preds[, intersect(names(preds), prCols), with = FALSE]
-
-        comps <- rbindlist(lapply(split(scrP, by = "group"), function(scrRow)
+        predCols <- c("name", "InChIKey", "formula", "mass", "RTDir")
+        preds <- preds[, intersect(names(preds), predCols), with = FALSE]
+        
+        return(rbindlist(lapply(precFGs, function(precFG)
         {
             # UNDONE: do more checks etc
             
@@ -123,35 +115,36 @@ generateComponentsTPs <- function(fGroups, pred, MSPeakLists, adduct, mzWindow =
             # merge rows with duplicate fGroups, for instance, caused by different TPs with equal mass
             ret[, TP_name := paste0(TP_name, collapse = ","), by = "group"]
             ret <- unique(ret, by = "group")
-
+            
             # dummy intensity value so e.g. plotSpec works            
             ret[, intensity := 1]
             
             if (minRTDiff > 0)
             {
-                rtDiffs <- ret$rt - gInfo[scrRow$group, "rts"]
+                rtDiffs <- gInfoTPs[ret$group, "rts"] - gInfoPrec[precFG, "rts"]
                 ret <- ret[RTDir == 0 | abs(rtDiffs) <= minRTDiff | (rtDiffs < 0 & RTDir < 0) | (rtDiffs > 0 & RTDir > 0)]
             }
-
-            precMSMS <- MSPeakLists[[scrRow$group]][["MSMS"]]
+            
+            precMSMS <- MSPeakLists[[precFG]][["MSMS"]]
             if (!is.null(precMSMS))
             {
                 getSim <- function(g, shift)
                 {
                     TPMSMS <- MSPeakLists[[g]][["MSMS"]]
-                    if (!is.null(TPMSMS)) # UNDONE: make some arguments configurable
+                    if (!is.null(TPMSMS))
                     {
+                        # UNDONE: make some arguments configurable (eg omit precursor)
+                        
                         if (shift)
-                            shift <- diff(gInfo[c(scrRow$group, g), "mzs"])
+                            shift <- diff(gInfoPrec[c(precFG, g), "mzs"])
                         else
                             shift <- 0
-                        
-                        return(spectrumSimilarity(MSPeakLists, scrRow$group, g, MSLevel = 2, doPlot = FALSE, mzShift = shift))
+                        return(spectrumSimilarity(MSPeakLists, precFG, g, MSLevel = 2, doPlot = FALSE, mzShift = shift))
                     }
-                        
+                    
                     return(NA)
                 }
-
+                
                 ret[, specSimilarity := sapply(group, getSim, shift = FALSE)]
                 ret[, specSimilarityShift := sapply(group, getSim, shift = TRUE)]
             }
@@ -159,9 +152,9 @@ generateComponentsTPs <- function(fGroups, pred, MSPeakLists, adduct, mzWindow =
                 ret[, c("specSimilarity", "specSimilarityShift") := NA]
             
             return(ret)
-        }), idcol = "precursor_group")
+        }), idcol = "precursor_group"))
     }), idcol = "precursor_susp_name")
-
+    
     if (nrow(compTab) > 0)
     {
         compTab[, name := paste0("CMP", .GRP), by = c("precursor_susp_name", "precursor_group")]

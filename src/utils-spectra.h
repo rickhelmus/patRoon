f75f2da57a2e8adcd0ea2c1f6f49bd4fa39c5dfa
Rick Helmus <r.helmus@uva.nl>
2020-09-30 16:26:52 +0200

    spelling: simularity --> similarity

--- src/utils-spectra.h
+++ src/utils-spectra.h
@@ -8,7 +8,7 @@ struct Spectrum
     std::vector<double> mzs, intensities;
 };
 
-double doCalcSpecSimularity(Spectrum sp1, Spectrum sp2, const std::string &method,
+double doCalcSpecSimilarity(Spectrum sp1, Spectrum sp2, const std::string &method,
                             const std::string &shift, double precDiff,
                             double mzWeight, double intWeight, double mzWindow);
 

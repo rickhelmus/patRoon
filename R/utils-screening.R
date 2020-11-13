825cad1f115302e94df69bd5548d3498f7c85858
Rick Helmus <r.helmus@uva.nl>
2020-11-13 16:22:30 +0100

    remove leftover

--- R/utils-screening.R
+++ R/utils-screening.R
@@ -241,8 +241,7 @@ genIDLevelRulesFile <- function(out, inLevels = NULL, exLevels = NULL)
         if (!is.null(exLevels))
             rules <- rules[!grepl(exLevels, names(rules))]
         # UNDONE: this quotes ID levels without sub-level, fix?
-        yaml::write_yaml(rules, out, indent = 4,
-                         handlers = list(int = function(x) { class(x) <- "verbatim"; x }))
+        yaml::write_yaml(rules, out, indent = 4)
     }
     invisible(NULL)
 }

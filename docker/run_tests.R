e8ef5737b7d698e634430ceb079772f2c0f881a6
Rick Helmus <r.helmus@uva.nl>
2020-11-16 15:12:06 +0100

    small testthat fixes for CircleCI [skip appveyor]

--- docker/run_tests.R
+++ docker/run_tests.R
@@ -1,11 +1,11 @@
 # unfortunately vdiffr doesn't allow to specify the deps file name.
 # file.rename("tests/figs/deps-docker.txt", "tests/figs/deps.txt")
 
-cat("options(patRoon.progress.opts = list(style = 1))", file = "~/.Rprofile", append = TRUE)
+options(patRoon.progress.opts = list(style = 1))
 
 # return failure exit code when tests fail: https://github.com/r-lib/testthat/issues/515
 tret <- as.data.frame(devtools::test(reporter = MultiReporter$new(list(SummaryReporter$new(),
-                                                                       JunitReporter$new(file = "junit.xml")))))
+                                                                       JunitReporter$new(file = "~/junit.xml")))))
 print(tret)
 if (sum(tret$failed) > 0 || any(tret$error))
     q(status = 1)

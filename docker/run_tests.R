7598d3f58b0627b4f310ffd8a81468fe6e651fcf
Rick Helmus <r.helmus@uva.nl>
2020-11-16 14:09:52 +0100

    updated tests

--- docker/run_tests.R
+++ docker/run_tests.R
@@ -1,7 +1,7 @@
 # unfortunately vdiffr doesn't allow to specify the deps file name.
 # file.rename("tests/figs/deps-docker.txt", "tests/figs/deps.txt")
 
-cat("options(patRoon.progress.opts = list(style = 1))", file = ".Rprofile", append = TRUE)
+cat("options(patRoon.progress.opts = list(style = 1))", file = "~/.Rprofile", append = TRUE)
 
 # return failure exit code when tests fail: https://github.com/r-lib/testthat/issues/515
 tret <- as.data.frame(devtools::test(reporter = MultiReporter$new(list(SummaryReporter$new(),

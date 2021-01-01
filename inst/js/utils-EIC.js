6a58851622c9d7f180895b6324f95fdb4a5ca1f5
Rick Helmus <r.helmus@uva.nl>
2021-01-01 20:51:58 +0100

    show popup on page leave when session was changed

--- inst/js/utils-EIC.js
+++ inst/js/utils-EIC.js
@@ -14,3 +14,20 @@ Shiny.addCustomMessageHandler("toggleFGroupRow", function(row)
     var ht = getGroupHot();
     ht.setDataAtCell(row - 1, 1, !ht.getDataAtCell(row - 1, 1));
 });
+
+// show popup if something was changed and user is about to leave...
+// based on https://stackoverflow.com/q/55048802
+var sessionChanged = false;
+Shiny.addCustomMessageHandler("setSessionChanged", function(changed) { sessionChanged = changed; });
+window.addEventListener('beforeunload', function(e)
+{
+    // from https://stackoverflow.com/a/61404006
+    if (sessionChanged)
+    {
+        e.preventDefault();
+        e.returnValue = '';
+        return;
+    }
+
+    delete e['returnValue'];
+});

b987cb36a095dcceb457958f67379e610b0d50a5
Rick Helmus <r.helmus@uva.nl>
2021-01-21 13:22:24 +0100

    finished most generic and features selection UI code

--- R/check_features.R
+++ R/check_features.R
@@ -1,20 +1,21 @@
 #' @include main.R
 #' @include feature_groups.R
+#' @include check_ui.R
 
-getUISettingsPath <- function()
+getUISettingsPathOld <- function()
 {
     dirPath <- RUserDir("patRoon", "config")
     mkdirp(dirPath)
     return(file.path(dirPath, "check_features.yml"))
 }
 
-getUISettings <- function()
+getUISettingsOld <- function()
 {
-    path <- getUISettingsPath()
+    path <- getUISettingsPathOld()
     if (!file.exists(path))
     {
-        ret <- getDefaultUISettings()
-        saveUISettings(ret)
+        ret <- getDefaultUISettingsOld()
+        saveUISettingsOld(ret)
     }
     else
         ret <- yaml::read_yaml(path, eval.expr = FALSE)
@@ -22,20 +23,20 @@ getUISettings <- function()
     return(ret)
 }
 
-getDefaultUISettings <- function()
+getDefaultUISettingsOld <- function()
 {
     return(list(retUnit = "sec", featQuantity = "intensity", fGroupQuantity = "average",
                 fGroupColumns = c("retMZ", "estIDLevel", "totalScore"),
                 featureColumns = c("retMZ", "quantity", "totalScore")))
 }
 
-saveUISettings <- function(settings)
+saveUISettingsOld <- function(settings)
 {
     settings$version <- 1 # just store for now, in case if ever needed in the future
     yaml::write_yaml(settings, getUISettingsPath(), indent = 4)
 }
 
-getCheckFeaturesSessionPath <- function(session)
+getCheckFeaturesSessionPathOld <- function(session)
 {
     outp <- file.path("sessions", "features")
     mkdirp(outp)
@@ -43,7 +44,7 @@ getCheckFeaturesSessionPath <- function(session)
 }
 
 #' @export
-importCheckFeaturesSession <- function(sessionIn, sessionOut, fGroups, overWrite = FALSE)
+importCheckFeaturesSessionOld <- function(sessionIn, sessionOut, fGroups, overWrite = FALSE)
 {
     # UNDONE: docs
     
@@ -325,7 +326,7 @@ setMethod("checkFeatures", "featureGroups", function(fGroups, session, rtWindow,
                     outsideClickDeselects = FALSE, manualColumnResize = TRUE,
                     rowHeaders = NULL)
     
-    settings <- getUISettings()
+    settings <- getUISettingsOld()
     
     curSession <- NULL
     sessionChanged <- FALSE
@@ -416,7 +417,7 @@ setMethod("checkFeatures", "featureGroups", function(fGroups, session, rtWindow,
                 reAddHOT("fGroupsHot")
             if (!isTRUE(all.equal(curSettings$featureColumns, settings$featureColumns)))
                 reAddHOT("featuresHot")
-            saveUISettings(settings)
+            saveUISettingsOld(settings)
         }
         
         syncInputSettings <- function()
@@ -685,7 +686,7 @@ setMethod("checkFeatures", "featureGroups", function(fGroups, session, rtWindow,
         })
         
         observeEvent(input$resetSettings, {
-            doApplySettings(getDefaultUISettings())
+            doApplySettings(getDefaultUISettingsOld())
             syncInputSettings()
             shinyjs::disable("saveApplySettings")
         })
@@ -823,11 +824,315 @@ setMethod("checkFeatures", "featureGroups", function(fGroups, session, rtWindow,
     runApp(shinyApp(getCheckFeatsUI(settings), server))
 })
 
+
+checkFeaturesInterface <- setRefClass("checkFeaturesInterface", contains = "checkUIInterface",
+                                      fields = c(fGroups = "featureGroups", EICsTopMost = "list",
+                                                 EICsTopMostRG = "list", EICsAll = "list",
+                                                 EICPreviews = "list"))
+
+checkFeaturesInterface$methods(
+    
+    UITitle = function() "Check features tool",
+    resetSecondaryUITitle = function() "Enable all features for all groups",
+    primaryTabTopUI = function()
+    {
+        fillRow(
+            width = 250,
+            flex = c(NA, 1),
+            fillRow(
+                width = 75,
+                div(style = "margin-top: 8px", HTML("<strong>Plot mode</strong>"))
+            ),
+            selectInput("fGroupPlotMode", NULL,
+                        c("Top most group" = "topMost",
+                          "Top most replicates" = "topMostByRGroup",
+                          "All" = "all"), "topMost")
+        )
+    },
+    settingsTabUI = function(settings)
+    {
+        fillRow(
+            height = 200,
+            
+            fillCol(
+                flex = NA,
+                radioButtons("retUnit", "Retention time unit", c("Seconds" = "sec", "Minutes" = "min"),
+                             settings$retUnit, inline = TRUE),
+                radioButtons("featQuantity", "Feature quantity", c("Peak intensity" = "intensity",
+                                                                   "Peak area" = "area"),
+                             settings$featQuantity, inline = TRUE),
+                radioButtons("fGroupQuantity", "Reported feature group quantities",
+                             c("Max" = "max", "Mean" = "average", "All" = "all"),
+                             settings$fGroupQuantity, inline = TRUE)
+            ),
+            fillRow(
+                checkboxGroupInput("fGroupColumns", "Feature groub table columns",
+                                   c("Retention time & m/z" = "retMZ",
+                                     "EIC preview" = "EICPreview",
+                                     "Suspect properties (RT, m/z, fragments)" = "suspProp",
+                                     "Estimated suspect identification level" = "estIDLevel",
+                                     "Other suspect annotations" = "suspOther",
+                                     "Total score" = "totalScore",
+                                     "Other scores" = "otherScores"),
+                                   settings$fGroupColumns)
+            ),
+            fillRow(
+                checkboxGroupInput("featureColumns", "Feature table columns",
+                                   c("Retention time & m/z" = "retMZ",
+                                     "Replicate group" = "rGroup",
+                                     "Blank" = "blank",
+                                     "Quantity" = "quantity",
+                                     "RT and m/z range" = "rtMZRange",
+                                     "Total score" = "totalScore",
+                                     "Other scores" = "otherScores"),
+                                   settings$featureColumns)
+            )
+        )
+    },
+    
+    primaryTab = function() "Feature groups",
+    secondaryTab = function() "Features",
+    
+    defaultUISettings = function()
+    {
+        return(list(retUnit = "sec", featQuantity = "intensity", fGroupQuantity = "average",
+                    fGroupColumns = c("retMZ", "estIDLevel", "totalScore"),
+                    featureColumns = c("retMZ", "quantity", "totalScore")))
+    },
+    UISettingsFileName = function() "check_features.yml",
+    
+    initReactiveValues = function(rValues)
+    {
+        rValues$fGroupPlotMode <- "topMost"
+        return(rValues)
+    },
+    
+    settingsChangedExpression = function(input)
+    {
+        input$retUnit; input$featQuantity; input$fGroupQuantity; input$fGroupColumns; input$featureColumns
+    },
+    primarySettingsChanged = function(cur, new)
+    {
+        return(!isTRUE(all.equal(cur$fGroupColumns, new$fGroupColumns)) ||
+                   cur$featQuantity != new$featQuantity ||
+                   cur$fGroupQuantity != new$fGroupQuantity)
+    },
+    secondarySettingsChanged = function(cur, new) !isTRUE(all.equal(cur$featureColumns, new$featureColumns)),
+    syncInputSettings = function(session, settings)
+    {
+        for (s in c("retUnit", "featQuantity", "fGroupQuantity"))
+            updateRadioButtons(session, s, selected = settings[[s]])
+        for (s in c("fGroupColumns", "featureColumns"))
+            updateCheckboxGroupInput(session, s, selected = settings[[s]])
+    },
+    
+    primaryTableData = function(rValues)
+    {
+        getScores <- any(c("totalScore", "otherScores") %in% rValues$settings$fGroupColumns)
+        args <- list(fGroups, areas = rValues$settings$featQuantity == "area",
+                     average = rValues$settings$fGroupQuantity == "average",
+                     qualities = if (getScores) "score" else FALSE)
+        if (isScreening(fGroups) && any(c("suspProp", "estIDLevel", "suspOther") %in% rValues$settings$fGroupColumns))
+            args <- c(args, list(collapseSuspects = NULL))
+        gData <- do.call(as.data.table, args)
+        
+        if (rValues$settings$fGroupQuantity == "max")
+        {
+            gData[, max_intensity := rowSums(.SD) / nrow(anaInfo), .SDcols = anaInfo$analysis]
+            gData <- gData[, (anaInfo$analysis) := NULL]
+        }
+        
+        if ("EICPreview" %in% rValues$settings$fGroupColumns)
+        {
+            gData[, EIC := sapply(gNames, function(g)
+            {
+                jsonlite::toJSON(list(values = EICPreviews[[g]]$intensity, xvalues = EICPreviews[[g]]$time,
+                                      options = list(type = "line", height = 50)))
+            })]
+            setcolorder(gData, c("group", "EIC"))
+        }
+        
+        if (!"retMZ" %in% rValues$settings$fGroupColumns)
+            gData[, c("ret", "mz") := NULL]
+        else if (rValues$settings$retUnit == "min")
+            gData[, ret := ret / 60]
+        
+        if (isScreening(fGroups))
+        {
+            if (!"suspProp" %in% rValues$settings$fGroupColumns)
+                gData[, (intersect(names(gData), c("susp_rt", "susp_mz", "fragments_mz", "fragments_formula"))) := NULL]
+            if (!"estIDLevel" %in% rValues$settings$fGroupColumns && !is.null(gData[["estIDLevel"]]))
+                gData[, "estIDLevel" := NULL]
+            if (!"suspOther" %in% rValues$settings$fGroupColumns)
+                gData[, (intersect(names(gData),
+                                   c("suspFormRank", "suspCompRank", "annSimForm", "annSimComp", "annSimBoth",
+                                     "maxFrags", "maxFragMatches", "maxFragMatchesRel"))) := NULL]
+        }
+        
+        if (getScores && hasFGroupScores(fGroups))
+        {
+            # scores were added by as.data.table(). Remove those we don't want.
+            if (!"totalScore" %in% rValues$settings$fGroupColumns)
+                gData[, totalScore := NULL]
+            if (!"otherScores" %in% rValues$settings$fGroupColumns)
+                gData[, (c(featureScoreNames(), featureGroupScoreNames())) := NULL]
+        }
+        
+        return(gData)
+    },
+    secondaryTableData = function(rValues)
+    {
+        fti <- groupFeatIndex(fGroups)[[rValues$currentPrimSel]]
+        ft <- featureTable(fGroups)[fti != 0]; ai <- analysisInfo(fGroups)[fti != 0, ]; fti <- fti[fti != 0]
+        feat <- rbindlist(Map(ft, fti, f = function(f, i) f[i]))
+        
+        fData <- data.table(analysis = ai$analysis)
+        if ("retMZ" %in% rValues$settings$featureColumns)
+            fData[, c("ret", "mz") := .(if (rValues$settings$retUnit == "min") feat$ret / 60 else feat$ret, feat$mz)]
+        if ("rGroup" %in% rValues$settings$featureColumns)
+            fData[, replicate_group := ai$group]
+        if ("blank" %in% rValues$settings$featureColumns)
+            fData[, blank := ai$blank]
+        if ("quantity" %in% rValues$settings$featureColumns)
+            fData[, quantity := if (rValues$settings$featQuantity == "intensity") feat$intensity else feat$area]
+        if ("rtMZRange" %in% rValues$settings$featureColumns)
+            fData[, c("retmin", "retmax", "mzmin", "mzmax") := .(feat$retmin, feat$retmax, feat$mzmin, feat$mzmax)]
+        if (hasFGroupScores(fGroups))
+        {
+            if ("otherScores" %in% rValues$settings$featureColumns)
+                fData[, (featureScoreNames()) := feat[, featureScoreNames(), with = FALSE]]
+            if ("totalScore" %in% rValues$settings$featureColumns)
+                fData[, totalScore := feat$totalScore]
+        }
+        
+        return(fData)
+    },
+    
+    doObserveEvents = function(input, rValues)
+    {
+        observeEvent(input$tabs, {
+            if (input$tabs != "Settings")
+            {
+                if (input$tabs == primaryTab())
+                    rValues$fGroupPlotMode <- input$fGroupPlotMode
+                else
+                    rValues$fGroupPlotMode <- "all"
+            }
+        })
+        
+        observeEvent(input$fGroupPlotMode, {
+            if ((input$fGroupPlotMode == "topMostByRGroup" && is.null(EICsTopMostRG)) ||
+                (input$fGroupPlotMode == "all" && is.null(EICsAll)))
+            {
+                not <- showNotification("Loading EICs...", duration = NULL, closeButton = FALSE, type = "message")
+                if (input$fGroupPlotMode == "topMostByRGroup")
+                    EICsTopMostRG <<- getEICsForFGroups(fGroups, rtWindow, mzExpWindow, 1, TRUE, TRUE)
+                else
+                    EICsAll <<- getEICsForFGroups(fGroups, rtWindow, mzExpWindow, NULL, FALSE, TRUE)
+                removeNotification(not)
+            }
+            rValues$fGroupPlotMode <- input$fGroupPlotMode
+        })
+    },
+    
+    plotChrom = function(rValues)
+    {
+        EICs <- switch(rValues$fGroupPlotMode,
+                       topMost = EICsTopMost,
+                       topMostByRGroup = EICsTopMostRG,
+                       all = EICsAll
+        )
+        if (length(EICs) == 0)
+            EICs <- NULL # not (yet) loaded, in this case plotChroms() will make its own but EICs must be NULL
+        
+        fg <- fGroups[, rValues$currentPrimSel]
+        if (rValues$fGroupPlotMode == "all") # UNDONE: also for rGroups top most somehow?
+            fg <- fg[rValues$secondarySelections[[rValues$currentPrimSel]]]
+        
+        withr::with_par(list(mar = c(4, 4, 0.1, 1), cex = 1.5), {
+            plotChroms(fg, EICs = EICs, colourBy = "rGroups", showPeakArea = TRUE,
+                       showFGroupRect = FALSE, title = "",
+                       topMost = if (rValues$fGroupPlotMode == "all") NULL else 1,
+                       topMostByRGroup = rValues$fGroupPlotMode == "topMostByRGroup",
+                       retMin = rValues$settings$retUnit == "min")
+        })
+    }
+)
+
+#' @export
+importCheckFeaturesSession <- function(sessionIn, sessionOut, fGroups, overWrite = FALSE)
+{
+    # UNDONE: docs
+    
+    checkmate::assertString(sessionIn, min.chars = 1)
+    pathIn <- paste0(sessionIn, ".Rds")
+    
+    ac <- checkmate::makeAssertCollection()
+    checkmate::assertFileExists(pathIn, "r", .var.name = "session", add = ac)
+    checkmate::assertString(sessionOut, min.chars = 1, add = ac)
+    checkmate::assertClass(fGroups, "featureGroups", add = ac)
+    checkmate::assertFlag(overWrite, add = ac)
+    checkmate::reportAssertions(ac)
+    
+    pathOut <- paste0(sessionOut, ".Rds")
+    
+    importCheckUISession(pathIn, pathOut, "feature groups", "analyses", names(fGroups), analyses(fGroups),
+                         overWrite = overWrite)
+}
+
+checkFeatures2 <- function(fGroups, session, rtWindow, mzExpWindow)
+{
+    # UNDONE: update docs
+    # UNDONE: remove mzExpWindow
+    
+    ac <- checkmate::makeAssertCollection()
+    assertCheckFeaturesSession(session, fGroups, mustExist = FALSE, add = ac)
+    aapply(checkmate::assertNumber, . ~ rtWindow + mzExpWindow, finite = TRUE, lower = 0,
+           fixed = list(add = ac))
+    checkmate::reportAssertions(ac)
+    
+    sessionPath <- paste0(session, ".Rds")
+    checkmate::assertPathForOutput(sessionPath, overwrite = TRUE, .var.name = "session")
+    
+    gNames <- names(fGroups)
+    fTable <- featureTable(fGroups)
+    ftind <- groupFeatIndex(fGroups)
+    
+    # UNDONE: make topMost/onlyPresent optional/interactive
+    EICsTopMost <- getEICsForFGroups(fGroups, rtWindow, mzExpWindow, topMost = 1, FALSE, onlyPresent = TRUE)
+    EICsTopMostRG <- EICsAll <- list()
+    
+    # format is in [[ana]][[fGroup]], since we only took top most intensive we can throw away the ana dimension
+    EICPreviews <- Reduce(modifyList, EICsTopMost)
+    EICPreviews <- Map(EICPreviews, names(EICPreviews), f = function(eic, grp)
+    {
+        anai <- which.max(fGroups[[grp]])
+        return(eic[numGTE(eic$time, fTable[[anai]]$retmin[ftind[[grp]][anai]]) &
+                       numLTE(eic$time, fTable[[anai]]$retmax[ftind[[grp]][anai]]), ])
+    })
+    
+    curSession <- NULL
+    if (file.exists(sessionPath))
+        curSession <- readRDS(sessionPath)
+    else
+    {
+        ef <- data.frame(name = analyses(fGroups))
+        ef[, gNames] <- TRUE
+        curSession <- list(primarySelections = gNames, secondarySelections = ef)
+    }
+    
+    int <- checkFeaturesInterface$new(fGroups = fGroups, EICsTopMost = EICsTopMost,
+                                      EICsTopMostRG = EICsTopMostRG, EICsAll = EICsAll,
+                                      EICPreviews = EICPreviews, primarySelections = gNames,
+                                      curSession = curSession, sessionPath = sessionPath)
+    return(runCheckUI(int))
+}
+
 convertQualitiesToMCData <- function(fGroups)
 {
     if (!hasFGroupScores(fGroups))
         stop("No feature qualities were calculated. Please run calculatePeakQualities() first.")
-
+    
     ret <- copy(groupQualities(fGroups))
     
     hasNA <- unique(unlist(lapply(ret, function(x) which(is.na(x)))))
@@ -865,7 +1170,7 @@ getMCTrainData <- function(fGroups, session)
 predictCheckFeaturesSession <- function(fGroups, session, model, overWrite = FALSE)
 {
     checkPackage("MetaClean")
-
+    
     checkmate::assertString(session, min.chars = 1)
     path <- getCheckFeaturesSessionPath(session)
     

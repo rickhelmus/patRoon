def1b99cd90581cf09446daf40aeb18e99ba32f3
Rick Helmus <r.helmus@uva.nl>
2021-02-12 13:26:07 +0100

    fixed test

--- tests/testthat/test-mspeaklists.R
+++ tests/testthat/test-mspeaklists.R
@@ -155,7 +155,7 @@ test_that("basic functionality", {
     expect_length(plists["nope"], 0)
     expect_equivalent(analyses(plists[1:2]), analyses(fGroups)[1:2])
     expect_equivalent(analyses(plists[analyses(fGroups)[1:2]]), analyses(fGroups)[1:2])
-    expect_equivalent(analyses(plists[c(FALSE, TRUE)]), analyses(fGroups)[2])
+    expect_equivalent(analyses(plists[c(FALSE, TRUE)]), analyses(fGroups)[c(FALSE, TRUE)])
     expect_equivalent(groupNames(plists[, 1:2]), groupNames(plists)[1:2])
     expect_equivalent(groupNames(plists[, groupNames(plists)[2:3]]), groupNames(plists)[2:3])
     expect_equivalent(groupNames(plists[, c(FALSE, TRUE)]), groupNames(plists)[c(FALSE, TRUE)])

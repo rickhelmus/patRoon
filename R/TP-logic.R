67a2d156a7923a52a9599ba68c2c34b86c5adf9d
Rick Helmus <r.helmus@uva.nl>
2021-02-26 16:50:30 +0100

    move reactions to sysdata

--- R/TP-logic.R
+++ R/TP-logic.R
@@ -7,32 +7,11 @@ NULL
 
 getTPLogicTransformations <- function()
 {
-    # UNDONE: cite 10.1021/acs.analchem.5b02905
-    
-    ret <- rbindlist(list(
-        list("hydroxylation", "O", "", -1),
-        list("demethylation", "", "CH2", -1),
-        list("deethylation", "", "C2H4", -1),
-        list("dehydrogenation", "H2", "", -1), # UNDONE: isn't this hydrogenation (and vice versa)?
-        list("hydrogenation", "", "H2", -1),
-        list("dehydration", "", "H2O", -1),
-        list("chlorine_reduction", "H", "Cl", -1),
-        list("acetylation", "C2H2O", "", -1),
-        list("deacetylation", "", "C2H2O", -1),
-        list("glucuronidation", "C6H8O6", "", -1),
-        list("deglucuronidation", "", "C8H8O6", 0), # UNDONE: or RTDir=1
-        list("sulfonation", "SO3", "", -1),
-        list("desulfonation", "", "SO3", -1)
-    ))
-    
-    # RTDir: -1: <= precursor; 0: no check; 1: >= precursor
-    setnames(ret, c("reaction", "add", "sub", "RTDir"))
-    
-    getMZ <- function(f) rcdk::get.formula(f)@mass
+    ret <- copy(patRoon:::TPsLogicReactions) # stored inside R/sysdata.rda
     
     ret[, deltaMZ := 0]
-    ret[nzchar(add), deltaMZ := sapply(add, getMZ)]
-    ret[nzchar(sub), deltaMZ := deltaMZ - sapply(sub, getMZ)]
+    ret[nzchar(add), deltaMZ := sapply(add, getFormulaMass)]
+    ret[nzchar(sub), deltaMZ := deltaMZ - sapply(sub, getFormulaMass)]
     
     return(ret[])
 }

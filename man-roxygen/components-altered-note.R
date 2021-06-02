0e84b1aefee579794a4a067381c5ce503d4bd9f8
Rick Helmus <r.helmus@uva.nl>
2021-06-02 15:14:39 +0200

    update componentsReduced docs

---
+++ man-roxygen/components-altered-note.R
@@ -0,0 +1,2 @@
+#' @note When the object is altered (\emph{e.g.} by filtering or subsetting it), methods that need the original
+#'   clustered data such as plotting methods do not work anymore and stop with an error.

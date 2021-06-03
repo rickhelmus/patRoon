894390016fd54ce04b9c526fe4462089180f8462
Rick Helmus <r.helmus@uva.nl>
2021-06-03 09:56:18 +0200

    updated docs for formulas/compounds

--- man-roxygen/plotSpec-args.R
+++ man-roxygen/plotSpec-args.R
@@ -1,5 +1,6 @@
-#' @param groupName The name of the feature group to which the candidate
+#' @param groupName The name of the feature group (or feature groups when comparing spectra) to which the candidate
 #'   belongs.
-#' @param MSPeakLists The \code{\link{MSPeakLists}} object that was used to
-#'   generate the candidate
+#' @param MSPeakLists The \code{\link{MSPeakLists}} object that was used to generate the candidate
+#' @param mincex The formula annotation labels are automatically scaled. The \code{mincex} argument forces a minimum
+#'   \code{cex} value for readability.
 

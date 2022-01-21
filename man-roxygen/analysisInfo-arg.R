997466917c9227415d9629107fc9a55df4f8f7de
Rick Helmus <r.helmus@uva.nl>
2022-01-21 15:17:49 +0100

    split findFeatures() docs

---
+++ man-roxygen/analysisInfo-arg.R
@@ -0,0 +1,1 @@
+#' @param analysisInfo A \code{data.frame} with \link[=analysis-information]{Analysis information}.
--- man-roxygen/generic-algo.R
+++ man-roxygen/generic-algo.R
@@ -2,5 +2,8 @@
 #'   functionality is provided by algorithm specific functions such as \code{<%=ex1%>} and \code{<%=ex2%>}. While these
 #'   functions may be called directly, \code{<%=func%>} provides a generic interface and is therefore usually preferred.
 #'
-#' <%=if (!exists("noParam")) paste("@param algorithm A character string describing the algorithm that should be
+#'   <%=if (!exists("noParam")) paste("@param algorithm A character string describing the algorithm that should be
 #'   used:", paste0("\\code{\"", unlist(strsplit(algos, ",")), "\"}", collapse = ", ")) %>
+#'
+#' @seealso The \code{\link{<%=ret%>}} output class and its methods and the algorithm specific functions:
+#'   <%=paste0("\\code{\\link{", func, unlist(strsplit(algosSuffix, ",")), "}}", collapse = ", ") %>

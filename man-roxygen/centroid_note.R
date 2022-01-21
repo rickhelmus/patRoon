a116d41da9ab5a549d8166ccea13cf4007f2323c
Rick Helmus <r.helmus@uva.nl>
2022-01-21 15:18:07 +0100

    add missing file

---
+++ man-roxygen/centroid_note.R
@@ -0,0 +1,4 @@
+#' @note In most cases it will be necessary to centroid your MS input files. The only exception is \command{Bruker},
+#'   however, you will still need centroided \file{mzXML}/\file{mzML} files for \emph{e.g.} plotting chromatograms. In
+#'   this case the centroided MS files should be stored in the same directory as the raw \command{Bruker} \file{.d}
+#'   files. The \code{\link{convertMSFiles}} function can be used to centroid data.

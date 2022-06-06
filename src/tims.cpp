38b8a6bcd888efe0df88d22b1a406264764bb11e
Rick Helmus <r.helmus@uva.nl>
2022-06-06 11:28:52 +0200

    embed hclust-cpp library

--- src/tims.cpp
+++ src/tims.cpp
@@ -1,2 +1,4 @@
 // Include here since there is no easy way to add it to the Makefile
 #include "opentims++/opentims_all.cpp"
+#include "hclust-cpp/fastcluster.h"
+#include "hclust-cpp/fastcluster.cpp"

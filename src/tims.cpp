390d47832050aa00575abc2327a1e52b0492b51a
Rick Helmus <r.helmus@uva.nl>
2022-06-06 12:10:58 +0200

    initial TIMS function prototypes [skip ci]

--- src/tims.cpp
+++ src/tims.cpp
@@ -1,4 +1,36 @@
-// Include here since there is no easy way to add it to the Makefile
-#include "opentims++/opentims_all.cpp"
-#include "hclust-cpp/fastcluster.h"
-#include "hclust-cpp/fastcluster.cpp"
+#include "opentims++/opentims_all.h"
+
+#include <Rcpp.h>
+
+// [[Rcpp::export]]
+void initBrukerLibrary(const std::string &path)
+{
+    setup_bruker(path);
+}
+
+// [[Rcpp::export]]
+Rcpp::List getTIMSFrame(const std::string &file, size_t frameID)
+{
+    TimsDataHandle TDH(file);
+    if (TDH.has_frame(frameID))
+    {
+        TimsFrame &frame = TDH.get_frame(frameID);
+        auto frameIDs = std::vector<uint32_t>(frame.num_peaks);
+        auto scanIDs = std::vector<uint32_t>(frame.num_peaks);
+        auto TOFs = std::vector<uint32_t>(frame.num_peaks);
+        auto intensities = std::vector<uint32_t>(frame.num_peaks);
+        auto mzs = std::vector<double>(frame.num_peaks);
+        auto invIonMobilities = std::vector<double>(frame.num_peaks);
+        auto retentionTimes = std::vector<double>(frame.num_peaks);
+        frame.save_to_buffs(frameIDs.data(), scanIDs.data(), TOFs.data(), intensities.data(), mzs.data(),
+                            invIonMobilities.data(), retentionTimes.data());
+        return Rcpp::List::create(Rcpp::Named("frame") = frameIDs,
+                                  Rcpp::Named("scan") = scanIDs,
+                                  Rcpp::Named("tof") = TOFs,
+                                  Rcpp::Named("intensity") = intensities,
+                                  Rcpp::Named("mz") = mzs,
+                                  Rcpp::Named("inv_ion_mobility") = invIonMobilities,
+                                  Rcpp::Named("retention_time") = retentionTimes);
+    }
+    return Rcpp::List();
+}

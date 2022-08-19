3fcc78763911c859d5e1a5b2f44f5d9b0692f58e
Rick Helmus <r.helmus@uva.nl>
2022-08-19 17:17:51 +0200

    WIP grouping for featuresSuspects

--- R/feature_groups-openms.R
+++ R/feature_groups-openms.R
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 #' @include features.R
+#' @include features-suspects.R
 #' @include feature_groups.R
 NULL
 
@@ -98,6 +99,18 @@ setMethod("groupFeaturesOpenMS", "features", function(feat, rtalign = TRUE, QT =
 
     return(ret)
 })
+
+#' @rdname groupFeaturesOpenMS
+#' @export
+setMethod("groupFeaturesOpenMS", "featuresSuspects", function(feat, rtalign = FALSE, ..., verbose = TRUE)
+{
+    aapply(checkmate::assertFlag, . ~ rtalign + verbose)
+    if (rtalign)
+        warning("Setting rtalign=TRUE most likely doesn't make sense for features from suspects.", call. = FALSE)
+    return(doGroupSuspects(feat, selectMethod("groupFeaturesOpenMS", "features"), rtalign = rtalign, ...,
+                           verbose = verbose))
+})
+
 
 generateConsensusXML <- function(feat, out, rtalign, QT, maxAlignRT, maxAlignMZ, maxGroupRT,
                                  maxGroupMZ, extraOptsRT, extraOptsGroup, verbose)

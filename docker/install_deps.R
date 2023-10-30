f1a651c846bfccf504cf536cb60c55018becef69
Rick Helmus <r.helmus@uva.nl>
2023-10-30 21:40:25 +0100

    cleanup [skip ci]

--- docker/install_deps.R
+++ docker/install_deps.R
@@ -1,8 +1,8 @@
 options(Ncpus = 8)
 
-install.packages("patRoonInst", repos = c('https://rickhelmus.r-universe.dev', 'https://cloud.r-project.org'))
+install.packages("remotes")
+remotes::install_github("rickhelmus/patRoonInst")
 
 # install everything, including patRoon. This will be removed afterward, but is handy to get all deps.
-patRoonInst::install(pkgs="patRoonExt", ask = FALSE, quiet = FALSE)
 patRoonInst::install(ask = FALSE, quiet = FALSE)
 remove.packages("patRoon")

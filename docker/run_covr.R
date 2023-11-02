899c6d9f5b4726588e6cda7151dfa4f6ed220551
Rick Helmus <r.helmus@uva.nl>
2023-11-02 10:39:05 +0100

    misc CI tests/fixes

--- docker/run_covr.R
+++ docker/run_covr.R
@@ -2,6 +2,6 @@
 options(covr.flags = list(CXXFLAGS = '', LDFLAGS = ''))
 options(patRoon.progress.opts = list(style = 1))
 
-install.packages("covr")
+install.packages(c("covr", "testthat", "vdiffr"))
 
 withr::with_envvar(list(NOT_CRAN = "true"), covr::codecov(quiet = FALSE, errorsAreFatal = FALSE, clean = FALSE))

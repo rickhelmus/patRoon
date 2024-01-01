bb2fd3555caa9f4bac2668c4cbaaf4f786e75ca5
Rick Helmus <r.helmus@uva.nl>
2024-01-01 21:04:20 +0100

    small SIRIUS fixes

--- R/formulas-sirius.R
+++ R/formulas-sirius.R
@@ -100,7 +100,7 @@ processSIRIUSFormulas <- function(msFName, outPath, adduct, ...)
 
             forms <- patRoon:::addMiscFormulaInfo(forms, adduct)
             
-            forms[, rank := NULL]
+            forms <- removeDTColumnsIfPresent(forms, "rank")
             
             forms[, fragInfo := Map(ion_formula, fragInfo, f = function(form, fi)
             {

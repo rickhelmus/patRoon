74041b9cbbaa2fa97ccbbed3198685c77065bb03
Rick Helmus <r.helmus@uva.nl>
2024-02-21 15:17:24 +0100

    minor consistency changes

--- tests/testthat/test-rawdata_parsers.R
+++ tests/testthat/test-rawdata_parsers.R
@@ -9,21 +9,21 @@ fList <- getTestFeatures(noiseThrInt = 1E5)
 fgOpenMS <- groupFeatures(fList, "openms")
 
 test_that("TICs", {
-    expect_s3_class(getTICs(anaInfoOne), "data.table")
+    checkmate::expect_data_table(getTICs(anaInfoOne))
     expect_equal(unique(getTICs(anaInfoOne, MSLevel = 2)[["MSLevel"]]), 2)
     expect_equal(colnames(getTICs(anaInfoOne)), c("analysis", "group", "ret", "MSLevel", "intensity"))
     expect_equal(nrow(getTICs(anaInfoOne)), 759)
     expect_lt(nrow(getTICs(anaInfoOne, retentionRange = c(10, 200))), 759)
-    expect_s3_class(getTICs(fList, MSLevel = 2), "data.table")
-    expect_s3_class(getTICs(fgOpenMS), "data.table")
+    checkmate::expect_data_table(getTICs(fList, MSLevel = 2))
+    checkmate::expect_data_table(getTICs(fgOpenMS))
 })
 
 test_that("BPCs", {
-    expect_s3_class(getBPCs(anaInfoOne), "data.table")
+    checkmate::expect_data_table(getBPCs(anaInfoOne))
     expect_equal(colnames(getBPCs(anaInfoOne)), c("analysis", "group", "ret", "MSLevel", "mz", "intensity"))
     expect_equal(nrow(getBPCs(anaInfoOne)), 759)
-    expect_s3_class(getBPCs(fList), "data.table")
-    expect_s3_class(getBPCs(fgOpenMS), "data.table")
+    checkmate::expect_data_table(getBPCs(fList))
+    checkmate::expect_data_table(getBPCs(fgOpenMS))
 })
 
 test_that("test plot TICs and BPCs", {

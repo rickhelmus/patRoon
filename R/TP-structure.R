10a1d7d36d919646ead5e5e5cbf6a4ed303197f9
Rick Helmus <r.helmus@uva.nl>
2024-03-03 20:24:57 +0100

    cleanup

--- R/TP-structure.R
+++ R/TP-structure.R
@@ -212,9 +212,6 @@ setMethod("filter", "transformationProductsStructure", function(obj, ..., remove
             obj <- delete(obj, j = function(tab, ...) !pred(tab$similarity))
         }
         
-        obj@products <- pruneList(obj@products, checkZeroRows = TRUE)
-        obj@parents <- obj@parents[name %in% names(obj@products)]
-        
         saveCacheData("filterTPs", obj, hash)
     }
     

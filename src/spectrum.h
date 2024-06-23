1e6f73594a33a32c1d3e8eab2e7d2a5b87174247
Rick Helmus <r.helmus@uva.nl>
2024-06-23 21:27:36 +0200

    WIP general spectrum class [skip ci]

---
+++ src/spectrum.h
@@ -0,0 +1,33 @@
+#ifndef PATROON_SPECTRUM_H
+#define PATROON_SPECTRUM_H
+
+#include <vector>
+
+class Spectrum
+{
+public:
+    using NumVecType = std::vector<double>;
+    
+private:
+    // UNDONE: should intensities be double or int?
+    NumVecType mzs, intensities;
+    
+public:
+    Spectrum() = default;
+    Spectrum(const NumVecType &m, const NumVecType &i) : mzs(m), intensities(i) { }
+    Spectrum(NumVecType::size_type size) : mzs(size), intensities(size) { }
+    
+    const auto &getMZs(void) const { return mzs; }
+    const auto &getIntensities(void) const { return intensities; }
+    
+    void append(double mz, double inten);
+    void append(const Spectrum &sp);
+    void setPeak(NumVecType::size_type i, double mz, unsigned inten) { mzs[i] = mz; intensities[i] = inten; }
+    
+    auto size(void) const { return mzs.size(); }
+    bool empty(void) const { return mzs.empty(); }
+    void resize(NumVecType::size_type s) { mzs.resize(s); intensities.resize(s); }
+    void clear(void) { mzs.clear(); intensities.clear(); }
+};
+
+#endif

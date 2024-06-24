0d2843f28029b643c1afa70f690fc4cdbe306b39
Rick Helmus <r.helmus@uva.nl>
2024-06-24 20:21:54 +0200

    Spectrum --> SpectrumRaw

--- src/spectrum-raw.cpp
+++ src/spectrum-raw.cpp
@@ -1,11 +1,11 @@
-#include "spectrum.h"
+#include "spectrum-raw.h"
 
-void Spectrum::append(double mz, double inten)
+void SpectrumRaw::append(double mz, double inten)
 {
     mzs.push_back(mz);
     intensities.push_back(inten);
 }
-void Spectrum::append(const Spectrum &sp)
+void SpectrumRaw::append(const SpectrumRaw &sp)
 {
     mzs.insert(mzs.end(), sp.getMZs().begin(), sp.getMZs().end());
     intensities.insert(intensities.end(), sp.getIntensities().begin(), sp.getIntensities().end());

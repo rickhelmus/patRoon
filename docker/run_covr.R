c916a1c6fd29e7f43280b5472447709090d39c68
Rick Helmus <r.helmus@uva.nl>
2024-06-27 12:53:27 +0200

    try covr with testthat [skip ci]

--- docker/run_covr.R
+++ docker/run_covr.R
@@ -4,4 +4,5 @@ options(patRoon.progress.opts = list(style = 1))
 
 install.packages(c("covr", "testthat", "vdiffr"))
 
-withr::with_envvar(list(NOT_CRAN = "true"), covr::codecov(quiet = FALSE, errorsAreFatal = FALSE, clean = FALSE))
+withr::with_envvar(list(NOT_CRAN = "true"), covr::codecov(quiet = FALSE, errorsAreFatal = FALSE, clean = FALSE,
+                                                          type = "none", code = { testthat::test_package("patRoon") }))

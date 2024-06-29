813d5647163597dc6b53132c3856ef1fb942170c
Rick Helmus <r.helmus@uva.nl>
2024-06-29 20:33:55 +0200

    consistent numeric types

--- src/spectrum-raw.h
+++ src/spectrum-raw.h
@@ -1,36 +1,42 @@
 #ifndef PATROON_SPECTRUM_RAW_H
 #define PATROON_SPECTRUM_RAW_H
 
+#include <stddef.h>
 #include <vector>
+
+namespace SpectrumRawTypes {
+
+using Time = float;
+using Mass = float;
+using Intensity = float; // UNDONE: or unsigned?
+
+}
 
 class SpectrumRaw
 {
-public:
-    using NumVecType = std::vector<double>;
-    
-private:
-    // UNDONE: should intensities be double or int?
-    float time;
-    NumVecType mzs, intensities;
+    SpectrumRawTypes::Time time;
+    std::vector<SpectrumRawTypes::Mass> mzs;
+    std::vector<SpectrumRawTypes::Intensity> intensities;
     
 public:
-    SpectrumRaw() = default;
-    SpectrumRaw(float t, const NumVecType &m, const NumVecType &i) : time(t), mzs(m), intensities(i) { }
-    SpectrumRaw(NumVecType::size_type size) : mzs(size), intensities(size) { }
+    SpectrumRaw(void) = default;
+    SpectrumRaw(SpectrumRawTypes::Time t, const std::vector<SpectrumRawTypes::Mass> &m,
+                const std::vector<SpectrumRawTypes::Intensity> &i) : time(t), mzs(m), intensities(i) { }
+    SpectrumRaw(SpectrumRawTypes::Time t, size_t size) : time(t), mzs(size), intensities(size) { }
     
-    float getTime(void) const { return time; }
+    SpectrumRawTypes::Time getTime(void) const { return time; }
     const auto &getMZs(void) const { return mzs; }
     const auto &getIntensities(void) const { return intensities; }
     
-    void setTime(float t) { time = t; }
-    void append(double mz, double inten);
+    void setTime(SpectrumRawTypes::Time t) { time = t; }
+    void append(SpectrumRawTypes::Mass mz, SpectrumRawTypes::Intensity inten);
     void append(const SpectrumRaw &sp);
-    void setPeak(NumVecType::size_type i, double mz, unsigned inten) { mzs[i] = mz; intensities[i] = inten; }
+    void setPeak(size_t i, SpectrumRawTypes::Mass mz, SpectrumRawTypes::Intensity inten) { mzs[i] = mz; intensities[i] = inten; }
     
     auto size(void) const { return mzs.size(); }
     bool empty(void) const { return mzs.empty(); }
-    void resize(NumVecType::size_type s) { mzs.resize(s); intensities.resize(s); }
-    void clear(void) { mzs.clear(); intensities.clear(); }
+    void resize(size_t s) { mzs.resize(s); intensities.resize(s); }
+    void clear(void) { time = 0.0f; mzs.clear(); intensities.clear(); }
 };
 
 #endif

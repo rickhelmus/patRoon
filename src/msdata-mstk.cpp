bd85495109dc76bf11c09b6a7b58698a5bf2b04d
Rick Helmus <r.helmus@uva.nl>
2024-07-12 21:29:50 +0200

    make MSReadBakcendMSTK::doGetSpectrumRawMetadata() multithreaded [skip ci]

--- src/msdata-mstk.cpp
+++ src/msdata-mstk.cpp
@@ -6,16 +6,35 @@
 #include "msdata-mstk.h"
 #include "spectrum-raw.h"
 
+namespace {
+
+MSToolkit::MSReader getMSTKReader(void)
+{
+    MSToolkit::MSReader ret;
+    ret.addFilter(MSToolkit::MS1);
+    ret.addFilter(MSToolkit::MS2);
+    return ret;
+}
+
+int getMSTKLastScan(const std::string &file)
+{
+    // load first spectrum to get file size
+    auto rd = getMSTKReader();
+    MSToolkit::Spectrum firstSpec;
+    rd.readFile(file.c_str(), firstSpec, 0, true);
+    return rd.getLastScan();
+}
+
+}
+
+
 int MSReadBackendMSTK::backends = 0;
 
 // [[Rcpp::interfaces(r, cpp)]]
 
 MSReadBackend::ThreadDataType MSReadBackendMSTK::doGetThreadData(void) const
 {
-    auto ret = std::make_shared<MSToolkit::MSReader>();
-    ret->addFilter(MSToolkit::MS1);
-    ret->addFilter(MSToolkit::MS2);
-    return ret;
+    return std::make_shared<MSToolkit::MSReader>(getMSTKReader());
 }
 
 SpectrumRaw MSReadBackendMSTK::doReadSpectrum(const ThreadDataType &tdata, SpectrumRawTypes::Scan scan) const
@@ -39,85 +58,56 @@ const SpectrumRawMetadata &MSReadBackendMSTK::doGetSpectrumRawMetadata(void) con
     {
         metadataLoaded = true;
         
-        MSToolkit::MSReader reader;
-        reader.addFilter(MSToolkit::MS1);
-        reader.addFilter(MSToolkit::MS2);
-        MSToolkit::Spectrum spec;
-        
+        const size_t lastScan = getMSTKLastScan(getCurrentFile());
         SpectrumRawMetadata meta;
-        
-        while (reader.readFile(getCurrentFile().c_str(), spec, 0, true))
+        ThreadExceptionHandler exHandler;
+
+        // UNDONE: make num_threads configurable
+        #pragma omp parallel num_threads(12)
         {
-            const bool isMS1 = spec.getMsLevel() == 1;
+            auto rd = getMSTKReader();
+            MSToolkit::Spectrum spec;
+            SpectrumRawMetadata threadMeta;
             
-            SpectrumRawMetadataMS *curMS1MD = (isMS1) ? &meta.first : &meta.second;
-            curMS1MD->scans.push_back(spec.getScanNumber());
-            curMS1MD->times.push_back(spec.getRTime() * 60); // NOTE: MSTK takes minutes
-            curMS1MD->TICs.push_back(spec.getTIC());
-            curMS1MD->BPCs.push_back(spec.getBPI());
+            #pragma omp for schedule(static) nowait
+            for (size_t i=0; i<lastScan; ++i)
+            {
+                exHandler.run([&]
+                {
+                    rd.readFile(getCurrentFile().c_str(), spec, i, true);
+                    const bool isMS1 = spec.getMsLevel() == 1;
+                    
+                    SpectrumRawMetadataMS *curMS1MD = (isMS1) ? &threadMeta.first : &threadMeta.second;
+                    curMS1MD->scans.push_back(spec.getScanNumber());
+                    curMS1MD->times.push_back(spec.getRTime() * 60); // NOTE: MSTK takes minutes
+                    curMS1MD->TICs.push_back(spec.getTIC());
+                    curMS1MD->BPCs.push_back(spec.getBPI());
+                    
+                    assert(curMS1MD->scans.size() == curMS1MD->times.size());
+                    
+                    if (!isMS1)
+                        threadMeta.second.isolationRanges.emplace_back(NumRange<SpectrumRawTypes::Mass>(spec.getSelWindowLower(), spec.getSelWindowUpper()));
+                });
+            }
             
-            assert(curMS1MD->scans.size() == curMS1MD->times.size());
-            
-            if (!isMS1)
-                meta.second.isolationRanges.emplace_back(NumRange<SpectrumRawTypes::Mass>(spec.getSelWindowLower(), spec.getSelWindowUpper()));
+            #pragma omp for schedule(static) ordered
+            for(int i=0; i<getOMPNumThreads(); ++i)
+            {
+                exHandler.run([&]
+                {
+                    #pragma omp ordered
+                    {
+                        meta.first.append(threadMeta.first);
+                        meta.second.append(threadMeta.second);
+                    }
+                });
+            }
         }
+        
+        exHandler.reThrow();
         
         emplaceSpecMeta(std::move(meta));
     }
     
     return MSReadBackend::doGetSpectrumRawMetadata();
 }
-
-
-#if 0
-using namespace MSToolkit;
-
-// [[nope Rcpp::export]]
-void MSToolkitTest(const std::string &path, int startIndex, int stopIndex)
-{
-    MSReader r;
-    Spectrum s;
-    int j;
-    
-    r.addFilter(MS1);
-    r.addFilter(MS2);
-    r.addFilter(MSX);
-    r.addFilter(SRM);
-    
-    char nativeID[257];
-    for (int i=startIndex; i<=stopIndex; ++i)
-    {
-        if (!r.readFile(path.c_str(), s, i) || s.getScanNumber() == 0)
-        {
-            Rcpp::Rcout << "Abort: invalid idx\n";
-            return;
-        }
-            
-        Rcpp::Rcout << "scan: " << s.getScanNumber() << "\n";
-        
-        char szNativeID[128];
-        if (s.getNativeID(szNativeID, 128))
-            Rcpp::Rcout << "success:  scan " << s.getScanNumber() << "nativeID: " << szNativeID << "\n";
-        else
-            Rcpp::Rcout << "failure:  scan " << s.getScanNumber() << "\n";
-        
-        Rcpp::Rcout << "size: " << s.size() << "\n";
-        
-        s.getNativeID(nativeID, 256);
-        Rcpp::Rcout << nativeID << "\n";
-        Rcpp::Rcout << "S\t" << s.getScanNumber() << "\t" << s.getScanNumber();
-        for(j=0;j<s.sizeMZ();j++){
-            Rcpp::Rcout << "\t" << s.getMZ(j);
-        }
-        Rcpp::Rcout << "\n";
-        if(s.getRTime()>0) Rcpp::Rcout << "I\tRTime\t" << s.getRTime() << "\n";
-        for(j=0;j<s.sizeZ();j++){
-            Rcpp::Rcout << "Z\t" << s.atZ(j).z << "\t" << s.atZ(j).mh << "\n";
-        }
-        
-        /*for(j=0;j<s.size();j++){
-            Rcpp::Rcout << s.at(j).mz << "\t" << s.at(j).intensity << "\n";
-        }*/
-    }
-}
-#endif

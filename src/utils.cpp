dfb11bad8c25323fa02f7f7af496bfcc900aab5e
Rick Helmus <r.helmus@uva.nl>
2024-07-14 16:43:29 +0200

    move util template functions to separate file and WIP spectral averaging

--- src/utils.cpp
+++ src/utils.cpp
@@ -5,11 +5,6 @@
 #include <memory>
 #include <cmath>
 
-// UNDONE: for some reason we have to include Rcpp.h here to make clusterNums() work properly, what's going on here?!?!
-#include <Rcpp.h>
-
-#include "hclust-cpp/fastcluster.h"
-
 #include "utils.h"
 
 // ---
@@ -73,71 +68,6 @@ void normalizeNums(std::vector<double> &v)
         m = std::max(m, d);
     for (double &d : v)
         d /= m;
-}
-
-std::vector<int> clusterNums(const std::vector<double> &nums, clusterMethod method, double window)
-{
-    std::vector<int> ret(nums.size());
-    
-    if (method == clusterMethod::BIN || method == clusterMethod::DIFF)
-    {
-        const auto sortedNumInds = getSortedInds(nums);
-        
-        int curCluster = 0;
-        if (method == clusterMethod::BIN)
-        {
-            int curBin = -1;
-            for (auto i : sortedNumInds)
-            {
-                const int bin = static_cast<int>(nums[i] / window);
-                if (curBin == -1 || bin != curBin)
-                {
-                    if (curBin != -1)
-                        ++curCluster;
-                    curBin = bin;
-                }
-                ret[i] = curCluster;
-            }
-        }
-        else // clusterMethod::DIFF
-        {
-            int curBinSize = 0;
-            double binSum = 0;
-            for (auto i : sortedNumInds)
-            {
-                if (curBinSize == 0 || (nums[i] - (binSum / static_cast<double>(curBinSize))) > window)
-                {
-                    if (curBinSize > 0)
-                        ++curCluster;
-                    curBinSize = 1;
-                    binSum = nums[i];
-                }
-                ret[i] = curCluster;
-                binSum += nums[i];
-                ++curBinSize;
-            }
-        }
-    }
-    else // clusterMethod::HCLUST
-    {
-        // get distance matrix, derived from hclust-cpp example
-        const auto n = nums.size();
-        auto distm = std::make_unique<double[]>((n * (n-1)) / 2);
-        for (size_t i=0, k=0; i<n; ++i)
-        {
-            for (size_t j=i+1; j<n; ++j)
-            {
-                distm[k] = abs(nums[i] - nums[j]);
-                ++k;
-            }
-        }
-        auto merge = std::make_unique<int[]>(2 * (n-1));
-        auto height = std::make_unique<double[]>(n - 1);
-        hclust_fast(n, distm.get(), HCLUST_METHOD_COMPLETE, merge.get(), height.get());
-        cutree_cdist(n, merge.get(), height.get(), window, ret.data());
-    }
-
-    return ret;
 }
 
 clusterMethod clustMethodFromStr(const std::string &str)

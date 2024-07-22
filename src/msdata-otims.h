d37fc46ab558798dfcaf00adfdd6b3d679213c6a
Rick Helmus <r.helmus@uva.nl>
2024-07-22 20:12:23 +0200

    WIP OpenTIMS backend

---
+++ src/msdata-otims.h
@@ -0,0 +1,26 @@
+#ifndef PATROON_OTIMS_H
+#define PATROON_OTIMS_H
+
+#include <Rcpp.h>
+
+#include "msdata.h"
+#include "opentims++/opentims.h"
+
+#include <memory>
+
+class MSReadBackendOTIMS: public MSReadBackend
+{
+    std::unique_ptr<TimsDataHandle> handle;
+    
+    void doOpen(const std::string &file) override;
+    void doClose(void) override;
+    ThreadDataType doGetThreadData(void) const override;
+    SpectrumRaw doReadSpectrum(const ThreadDataType &tdata, SpectrumRawTypes::Scan scan) const override;
+    
+public:
+    void setSpecMetadata(const Rcpp::DataFrame &mdMS, const Rcpp::DataFrame &mdMSMS);
+};
+
+RCPP_EXPOSED_CLASS(MSReadBackendOTIMS)
+
+#endif

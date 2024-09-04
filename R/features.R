695153dbc9245364a61c3394abb7024aebe99e1b
Ricardo Cunha <64540212+ricardo-cunha@users.noreply.github.com>
2024-09-04 17:17:03 +0200

    Added FeaturesQAlgorithms
    First commit for qAlgorithms via external .exe

--- R/features.R
+++ R/features.R
@@ -493,8 +493,8 @@ findFeatures <- function(analysisInfo, algorithm, ..., verbose = TRUE)
 {
     ac <- checkmate::makeAssertCollection()
     analysisInfo <- assertAndPrepareAnaInfo(analysisInfo, add = ac)
-    checkmate::assertChoice(algorithm, c("bruker", "openms", "xcms", "xcms3", "envipick", "sirius", "kpic2", "safd"),
-                            add = ac)
+    checkmate::assertChoice(algorithm, c("bruker", "openms", "xcms", "xcms3", "envipick", "sirius", "kpic2", "safd",
+                            "qalgorithms"), add = ac)
     checkmate::assertFlag(verbose, add = ac)
     checkmate::reportAssertions(ac)
 
@@ -506,7 +506,8 @@ findFeatures <- function(analysisInfo, algorithm, ..., verbose = TRUE)
                 envipick = findFeaturesEnviPick,
                 sirius = findFeaturesSIRIUS,
                 kpic2 = findFeaturesKPIC2,
-                safd = findFeaturesSAFD)
+                safd = findFeaturesSAFD,
+                qalgorithms = findFeaturesQAlgorithms)
 
     f(analysisInfo, ..., verbose = verbose)
 }

58907d8cb4a84f2db121cb76b752d13243b2dbc7
Rick Helmus <r.helmus@uva.nl>
2024-10-04 07:57:25 +0200

    compilation fixes

--- tools/build_flags.R
+++ tools/build_flags.R
@@ -10,6 +10,7 @@ getFlags <- function(what)
         cat(Rmstoolkitlib::pkgconfig(what))
     }
     # NOTE: the use of an underscore or dash for the CPU arch seems a bit random so just check for both
-    if (Sys.info()["machine"] %in% c("x86-64", "x86_64") && Sys.info()["sysname"] %in% c("Windows", "Linux"))
+    if (what %in% c("PKG_CFLAGS", "PKG_CXXFLAGS") && Sys.info()["machine"] %in% c("x86-64", "x86_64") &&
+        Sys.info()["sysname"] %in% c("Windows", "Linux"))
         cat(" -DWITH_OTIMS")
 }

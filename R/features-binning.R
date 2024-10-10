f780d2c2f0088bf885b684fbc1af6a40a08ecd72
Rick Helmus <r.helmus@uva.nl>
2024-10-10 21:23:57 +0200

    cache results [skip ci]

--- R/features-binning.R
+++ R/features-binning.R
@@ -23,28 +23,53 @@ findFeaturesBinning <- function(analysisInfo, mzRange = c(50, 400), mzStep = 0.0
     checkmate::reportAssertions(ac)
     
     cacheDB <- openCacheDBScope()
-    
-    bins <- seq(mzRange[1], mzRange[2], by = mzStep * 0.5)
-    names(bins) <- paste0("bin_M", bins)
-    
-    if (verbose)
-        printf("Finding features in %d bins for %d analyses ...\n", length(bins), nrow(analysisInfo))
-
-    printf("Loading EICs...\n")
-    EICInfoList <- rep(list(data.table(mzmin = bins, mzmax = bins + mzStep, retmin = 0, retmax = 0)), nrow(analysisInfo))
-    allEICs <- doGetEICs(analysisInfo, EICInfoList, compress = FALSE, withBP = TRUE, cacheDB = cacheDB)
-    allEICs <- lapply(allEICs, setNames, names(bins))
-    
-    fList <- findPeaksInEICs(allEICs, findPeaksAlgo, withBP = TRUE, ..., parallel = parallel, cacheDB = cacheDB)
-    
-    fList <- lapply(fList, function(fTab)
+    baseHash <- makeHash(mzRange, mzStep, findPeaksAlgo, list(...))
+    anaHashes <- unlist(applyMSData(analysisInfo, showProgress = FALSE,
+                                    func = function(ana, path, ...) makeHash(baseHash, getMSDataFileHash(path))))
+    cachedData <- loadCacheData("featuresBinning", anaHashes, simplify = FALSE, dbArg = cacheDB)
+    if (!is.null(cachedData))
     {
-        # only keep those peaks with m/z in the "center" of the analyzed m/z range
-        fTab[between(mz, bins[EIC_ID] + mzStep/4, bins[EIC_ID] + mzStep/4*3) == TRUE][, EIC_ID := NULL]
-    })
+        cachedData <- pruneList(setNames(cachedData, names(anaHashes)[match(names(cachedData), anaHashes)]))
+        anaInfoTBD <- analysisInfo[!analysis %in% names(cachedData)]
+    }
+    else
+        anaInfoTBD <- analysisInfo
     
-    # UNDONE: use BP intensity?
+    fList <- list()
+    if (nrow(anaInfoTBD) > 0)
+    {
+        # UNDONE: also support other binning approaches?
+        bins <- seq(mzRange[1], mzRange[2], by = mzStep * 0.5)
+        names(bins) <- paste0("bin_M", bins)
+
+        if (verbose)
+            printf("Finding features in %d bins for %d analyses ...\n", length(bins), nrow(anaInfoTBD))
         
+        printf("Loading EICs...\n")
+        EICInfoList <- rep(list(data.table(mzmin = bins, mzmax = bins + mzStep, retmin = 0, retmax = 0)), nrow(anaInfoTBD))
+        allEICs <- doGetEICs(anaInfoTBD, EICInfoList, compress = FALSE, withBP = TRUE, cacheDB = cacheDB)
+        allEICs <- lapply(allEICs, setNames, names(bins))
+        
+        fList <- findPeaksInEICs(allEICs, findPeaksAlgo, withBP = TRUE, ..., parallel = parallel, cacheDB = cacheDB)
+        
+        fList <- lapply(fList, function(fTab)
+        {
+            # only keep those peaks with m/z in the "center" of the analyzed m/z range
+            fTab[between(mz, bins[EIC_ID] + mzStep/4, bins[EIC_ID] + mzStep/4*3) == TRUE][, EIC_ID := NULL]
+        })
+        
+        # UNDONE: use BP intensity?
+        
+        for (a in anaInfoTBD$analysis)
+            saveCacheData("featuresBinning", fList[[a]], anaHashes[[a]])
+    }
+    
+    if (!is.null(cachedData))
+    {
+        fList <- c(fList, cachedData)
+        fList <- fList[analysisInfo$analysis] # put original order
+    }
+    
     if (verbose)
     {
         printf("Done!\n")

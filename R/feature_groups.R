96ad41446cc7715589812dbc9c249e5d58133f73
Rick Helmus <r.helmus@uva.nl>
2024-11-14 16:12:04 +0100

    assignMobilityPeaks(): skip features with assigned mobility features, instead of selection callback

--- R/feature_groups.R
+++ R/feature_groups.R
@@ -1393,7 +1393,7 @@ setMethod("findMobilities", "featureGroups", function(fGroups, mobPeaksParam, mz
         return(fGroups) # nothing to do...
     
     fGroups@features <- assignFeatureMobilitiesPeaks(fGroups@features, mobPeaksParam, mzWindow, IMSWindow, clusterMethod,
-                                                    minIntensityIMS, maxMSRTWindow, NULL)
+                                                    minIntensityIMS, maxMSEICRTWindow)
     fGroups@features <- reintegrateMobilityFeatures(fGroups@features, EICRTWindow, peakRTWindow, calcArea,
                                                     chromPeaksParam, fallbackEIC, parallel)
     fGroups <- clusterFGroupMobilities(fGroups, IMSWindow, FALSE)

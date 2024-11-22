9d0e3f9d7efeefd846ab1a0a1a5c96052217ed49
Rick Helmus <r.helmus@uva.nl>
2024-11-22 20:55:05 +0100

    small updates/fixes

--- R/feature_groups-set.R
+++ R/feature_groups-set.R
@@ -344,7 +344,7 @@ setMethod("findMobilities", "featureGroupsSet", function(fGroups, mobPeaksParam,
         return(fGroups) # nothing to do...
     
     fGroups@features <- assignFeatureMobilitiesPeaks(fGroups@features, mobPeaksParam, mzWindow, IMSWindow, clusterMethod,
-                                                    minIntensityIMS, maxMSRTWindow)
+                                                     minIntensityIMS, maxMSRTWindow)
     fGroups@features <- reintegrateMobilityFeatures(fGroups@features, EICRTWindow, peakRTWindow, calcArea,
                                                     chromPeaksParam, fallbackEIC, parallel)
     fGroups <- clusterFGroupMobilities(fGroups, IMSWindow, TRUE)

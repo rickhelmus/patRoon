a2a0e580d529e65f11b55131f060f3088ea045b0
Rick Helmus <r.helmus@uva.nl>
2024-11-27 11:07:41 +0100

    features: remove mobilities slot, and add hasMobilities slot to better flag that mobilities were assigned in eg cases where all features were removed

--- R/utils-features.R
+++ R/utils-features.R
@@ -965,6 +965,8 @@ assignFeatureMobilitiesPeaks <- function(features, peaksParam, IMSWindow, cluste
 
     printf("Assigned %d mobility features.\n", countMobilityFeatures(features) - oldCount)
     
+    features@hasMobilities <- TRUE
+    
     saveCacheData("assignFeatureMobilitiesPeaks", features, hash)
     
     return(features)

c7e38c5c147b575e426be1caf28b85edd2c24a08
Rick Helmus <r.helmus@uva.nl>
2025-02-19 14:16:09 +0100

    small fix

--- R/utils-features.R
+++ R/utils-features.R
@@ -1251,7 +1251,10 @@ selectIMSFilterFeatures <- function(features, IMS)
             !is.na(fl$mobility) & fl$ims_parent_ID %chin% fl$ID
     })
     
-    return(clearMobilities(features))
+    if (isFALSE(IMS))
+        features <- clearMobilities(features)
+    
+    return(features)
 }
 
 expandTableForIMSFGroups <- function(tab, gInfo)

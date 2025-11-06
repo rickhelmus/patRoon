5e2dabb237416ee8862e07a835da35ffeca1eb38
Rick Helmus <r.helmus@uva.nl>
2025-11-06 15:38:52 +0100

    update covr

--- docker/run_covr.R
+++ docker/run_covr.R
@@ -3,7 +3,7 @@ options(covr.flags = list(CXXFLAGS = '', LDFLAGS = ''))
 options(patRoon.progress.opts = list(style = 1))
 
 install.packages(c("testthat", "vdiffr"))
-remotes::install_github("gergness/covr@live-console-update")
+remotes::install_github("rickhelmus/covr@live-console-update")
 
 withr::with_envvar(list(NOT_CRAN = "true"), covr::codecov(quiet = FALSE, errorsAreFatal = FALSE, clean = FALSE,
                                                           type = "none", code = 'testthat::test_package("patRoon")',

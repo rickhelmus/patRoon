fdad4e4faf8202efcb732eec9bbf3a3d123ef017
Rick Helmus <r.helmus@uva.nl>
2025-12-11 20:35:40 +0100

    WIP docker updates

--- docker/install_deps.R
+++ docker/install_deps.R
@@ -7,3 +7,6 @@ remotes::install_github("rickhelmus/patRoonInst")
 # install everything, including patRoon. This will be removed afterward, but is handy to get all deps.
 patRoonInst::install(ask = FALSE, quiet = FALSE)
 remove.packages("patRoon")
+
+# UNDONE
+install.packages("reticulate")

535ea1c29e13bb47430e27cd6721355c9571c265
rickhelmus <r.helmus@uva.nl>
2026-01-21 13:57:29 +0100

    updates for REUSE

--- inst/js/utils-report-legacy.js
+++ inst/js/utils-report-legacy.js
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2016 - 2025 Rick Helmus <r.helmus@uva.nl>
+// SPDX-FileCopyrightText: 2016-2026 Rick Helmus <r.helmus@uva.nl>
 //
 // SPDX-License-Identifier: GPL-3.0-only
 
--- tools/update-license-header.R
+++ tools/update-license-header.R
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2016 - 2025 Rick Helmus <r.helmus@uva.nl>
+# SPDX-FileCopyrightText: 2016-2026 Rick Helmus <r.helmus@uva.nl>
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
@@ -10,7 +10,7 @@ local({
         if (!is.null(exclude))
             paths <- setdiff(paths, exclude)
         args <- c("annotate",
-                  "--year=2016-2025",
+                  "--year=2016-2026",
                   "--copyright=Rick Helmus <r.helmus@uva.nl>",
                   "--merge-copyrights",
                   "--license=GPL-3.0-only")

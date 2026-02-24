41c37969958323264dc78a1ee61e046f2b9c00fb
Rick Helmus <r.helmus@uva.nl>
2026-02-24 13:06:00 +0100

    [skip ci]

--- docker/run_tests.R
+++ docker/run_tests.R
@@ -4,7 +4,7 @@
 install.packages(c("devtools", "vdiffr"))
 
 # NOTE: these things need to be set as env vars since parallel testthat seems to ignore options() and .Rprofile
-Sys.setenv(TESTTHAT_CPUS = 4)
+Sys.setenv(TESTTHAT_CPUS = 3)
 Sys.setenv(PATROON_MP_MAXPROCS = 2)
 Sys.setenv(PKG_BUILD_EXTRA_FLAGS = "false")
 

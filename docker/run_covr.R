ef687cb6df86013019e61640a27b285f2cb19c41
Rick Helmus <r.helmus@uva.nl>
2026-03-02 21:10:10 +0100

    .

--- docker/run_covr.R
+++ docker/run_covr.R
@@ -1,7 +1,13 @@
 # disable flags as otherwise GenForm doesn't compile
 options(covr.flags = list(CXXFLAGS = '', LDFLAGS = ''))
 
-Sys.setenv(TESTTHAT_CPUS = 3)
+Sys.setenv(TESTTHAT_CPUS = 2)
+Sys.setenv(PATROON_MP_MAXPROCS = 2)
+Sys.setenv(PKG_BUILD_EXTRA_FLAGS = "false")
+
+# HACK: trigger compile first. It seems that parallel testing triggers multiple compiles, resulting in random compile
+# errors.
+devtools::load_all()
 
 install.packages(c("testthat", "vdiffr"))
 remotes::install_github("rickhelmus/covr@live-console-update")

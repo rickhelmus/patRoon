5bb98ecf04dcbfa3d05f43d0cd57e3e9a936a573
rickhelmus <r.helmus@uva.nl>
2026-04-08 10:33:17 +0200

    attempt workaround for slow Zenodo day [skip ci]

--- docker/install_deps.R
+++ docker/install_deps.R
@@ -5,10 +5,9 @@ install.packages("remotes")
 remotes::install_github("rickhelmus/patRoonInst")
 
 # install everything, including patRoon. This will be removed afterward, but is handy to get all deps.
-patRoonInst::install(ask = FALSE, quiet = FALSE)
+patRoonInst::install(ask = FALSE, quiet = FALSE, ignorePkgs = "patRoonExt")
 # UNDONE: pull in other deps
 # ---
-remotes::install_github("rickhelmus/patRoonData@version30")
 remotes::install_github("rickhelmus/patRoonExt@version20")
 remotes::install_github("rickhelmus/patRoon@version30")
 # ---

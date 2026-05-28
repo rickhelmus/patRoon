9c2618875b77877a5856bef4cbce62e9599c44fc
rickhelmus <r.helmus@uva.nl>
2026-05-28 10:06:58 +0200

    re-add topMost for compounds

--- man-roxygen/comp_algo-args.R
+++ man-roxygen/comp_algo-args.R
@@ -1,4 +1,3 @@
-#' @param topMost Only keep this number of candidates (per feature group) with highest score. Set to \code{NULL} to
-#'   always keep all candidates, however, please note that this may result in significant usage of CPU/RAM resources for
-#'   large numbers of candidates.
+#' @param topMost Only keep this number of candidates (per feature group) with highest score. Setting this to a high
+#'   number may result in significant usage of CPU/RAM resources for large numbers of candidates.
 #' @param \dots \setsWF Further arguments passed to the non-sets workflow method.
